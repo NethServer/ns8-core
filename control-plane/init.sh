01ba266e0b2343c9923f7c6751282b2bdc4358a5
Davide Principi <davide.principi@nethesis.it>
2021-02-25 16:59:18 +0100

    Verbose extraction

--- control-plane/init.sh
+++ control-plane/init.sh
@@ -24,7 +24,7 @@ cplanedir="${installdir}/cplane"
 echo "Extracting control plane sources:"
 podman pull ghcr.io/nethserver/control-plane:latest
 cid=$(podman create ghcr.io/nethserver/control-plane:latest)
-podman export ${cid} | tar -C ${installdir} -x -f -
+podman export ${cid} | tar -C ${installdir} -x -v -f -
 podman rm -f ${cid}
 
 cp -f ${agentdir}/node-agent.service      /etc/systemd/system/node-agent.service
