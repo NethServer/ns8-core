13aed5f853247d799f4a847558e97c8a0ed588f8
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2021-03-01 17:19:18 +0100

    nextcloud: run container on build

--- data-plane/nextcloud/build.sh
+++ data-plane/nextcloud/build.sh
@@ -7,18 +7,18 @@ HOST=nextcloud.$(hostname -f)
 
 podman pod create --name $N -p 127.0.0.1:8181:80
 
-podman create -d --pod $N \
+podman run --pod $N \
   --name $N-db \
   -e MYSQL_ROOT_PASSWORD=nextcloud  -e MYSQL_DATABASE=nextcloud -e MYSQL_USER=nextcloud -e MYSQL_PASSWORD=nextcloud \
   -v $N-db-data:/var/lib/mysql \
   docker.io/mariadb:10
 
-podman create -d --pod $N \
+podman run -d --pod $N \
   --name $N-redis \
   -v $N-redis-data:/data \
   docker.io/redis:6-alpine --appendonly yes
 
-podman create -d --pod $N \
+podman run -d --pod $N \
   --name $N-app \
   -e MYSQL_DATABASE=nextcloud -e MYSQL_HOST=127.0.0.1 -e MYSQL_USER=nextcloud -e MYSQL_PASSWORD=nextcloud \
   -e NEXTCLOUD_ADMIN_USER=admin -e NEXTCLOUD_ADMIN_PASSWORD=Nethesis,1234 \
