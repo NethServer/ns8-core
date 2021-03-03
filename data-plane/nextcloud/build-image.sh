26a307039127a86f450913d505b3dbd3be335bcd
Giacomo Sanchietti <giacomo.sanchietti@gmail.com>
2021-03-03 12:11:57 +0100

    nextcloud: directly push to ghcr.io

--- data-plane/nextcloud/build-image.sh
+++ data-plane/nextcloud/build-image.sh
@@ -9,9 +9,4 @@ buildah copy "${container}" config /.config
 buildah config --entrypoint=/ "${container}"
 buildah commit "${container}" "${image}"
 
-echo
-echo "Access DigitalOcean control panel and download docker-config.json file from Container Registry."
-echo "Then publish the image with:"
-echo
-echo " export REGISTRY_AUTH_FILE=path-to/docker-config.json"
-echo " buildah push ${image} docker://ghcr.io/nethserver/${image}:latest"
+buildah --authfile /usr/local/etc/registry.json push nextcloud docker://ghcr.io/nethserver/nextcloud:latest
