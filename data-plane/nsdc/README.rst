82272cef1b1e202a80d6da452292464bd4318bba
Davide Principi <davide.principi@nethesis.it>
2021-03-09 10:10:49 +0100

    Add MODULE_ID parameter in rootfull module agent

--- data-plane/nsdc/README.rst
+++ data-plane/nsdc/README.rst
@@ -2,6 +2,9 @@
 
 This module runs as singleton rootfull instance.
 
+- *Rootfull* because Samba needs special privileges to store ACLs in the filesystem extended attributes
+- *Singleton* because Samba services are bound to a host IP address, to serve LAN clients
+
 Initialize the Redis DB with
 
-    HSET nsdc/module.env NSDC_IPADDRESS 10.133.0.5 EVENTS_IMAGE ghcr.io/nethserver/cplane-nsdc NSDC_HOSTNAME nsdc1.ad.dp.nethserver.net NSDC_NBDOMAIN AD NSDC_REALM AD.DP.NETHSERVER.NET NSDC_ADMINPASS Nethesis,1234
+    HSET nsdc0/module.env EVENTS_IMAGE ghcr.io/nethserver/cplane-nsdc:latest NSDC_IMAGE ghcr.io/nethserver/nsdc:latest IPADDRESS 10.133.0.5 HOSTNAME nsdc1.ad.dp.nethserver.net NBDOMAIN AD REALM AD.DP.NETHSERVER.NET ADMINPASS Nethesis,1234
--- data-plane/nsdc/build-image.sh
+++ data-plane/nsdc/build-image.sh
@@ -6,7 +6,7 @@ image1="cplane-nsdc"
 container=$(buildah from scratch)
 buildah copy "${container}" module-events /module-events
 buildah copy "${container}" nsdc.service /nsdc.service
-buildah config --entrypoint=/ "${container}"
+buildah config --entrypoint='["/"]' "${container}"
 buildah commit "${container}" "${image1}"
 
 image2="nsdc"
@@ -18,12 +18,12 @@ apt-get -y install samba winbind
 apt-get clean
 find /var/lib/apt/lists/ -type f -delete
 EOF
-buildah config --cmd=/init.sh "${container}"
-buildah copy "${container}" init.sh /init.sh
+buildah config --cmd='' --entrypoint='["/entrypoint.sh"]' "${container}"
+buildah copy "${container}" entrypoint.sh /entrypoint.sh
 buildah commit "${container}" "${image2}"
 
 echo
-echo "Publish the image with:"
+echo "Publish the images with:"
 echo
 echo " buildah push ${image1} docker://ghcr.io/nethserver/${image1}:latest"
 echo " buildah push ${image2} docker://ghcr.io/nethserver/${image2}:latest"
