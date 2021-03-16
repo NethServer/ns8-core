ef85af1a11bb6501b1878688e1cebd6cc9c19758
Davide Principi <davide.principi@nethesis.it>
2021-03-16 13:13:27 +0100

    Basic Dovecot configuration

---
+++ mail/README.md
@@ -0,0 +1,8 @@
+# mail
+
+Mail services
+
+## Installation
+
+    HSET module/mail0/module.env HOSTNAME mail.example.com LDAPHOST 10.133.0.5 LDAPPORT 636 LDAPSSL on BINDDN %n@AD.DP.NETHSERVER.NET
+
--- mail/build-image.sh
+++ mail/build-image.sh
@@ -2,11 +2,20 @@
 
 set -e
 
-image="mail"
-container=$(buildah from docker.io/library/python:3.9-alpine)
+image="mail-dovecot"
+container=$(buildah from docker.io/library/alpine:3.13)
 
-buildah copy "${container}" config /.config
+buildah run "${container}" /bin/sh <<EOF
+addgroup -S vmail
+adduser -G vmail -h /var/lib/vmail -S vmail
+apk add --no-cache dovecot dovecot-ldap dovecot-pigeonhole-plugin dovecot-submissiond
+find /var/cache/apk /var/lib/apk -type f -delete
+EOF
+buildah copy "${container}" config /srv/mail/.config
+buildah add "${container}" dovecot/ /etc/dovecot/
 buildah config \
+    --cmd='["/bin/sh", "/etc/dovecot/start.sh"]' \
+    --label "org.nethserver.initroot=/srv/mail" \
     "${container}"
 buildah commit "${container}" "${image}"
 
