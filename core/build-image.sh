1537b517fe945b18ee928e1bb95341423aca1897
Davide Principi <davide.principi@nethesis.it>
2021-03-23 15:17:28 +0100

    Add Podman system-wide defaults

--- core/build-image.sh
+++ core/build-image.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 set -e
-
-# export IMAGE_TAG=$(git rev-parse --abbrev-ref HEAD)
-image_tag=${IMAGE_TAG:-latest}
+messages=("Publish the image with:" "")
+repobase="ghcr.io/nethserver"
 
 container=$(buildah from scratch)
+reponame="core"
 
+buildah copy ${container} etc /etc
 buildah copy ${container} agent /agent
 buildah copy ${container} module.skel /module.skel
 buildah config --entrypoint=/ ${container}
-buildah commit ${container} core
+buildah commit "${container}" "${repobase}/${reponame}"
+messages+=(" buildah push ${repobase}/${reponame} docker://${repobase}/${reponame}:latest")
 
-echo
-echo "Publish the image with:"
-echo
-echo " buildah push core docker://ghcr.io/nethserver/core:${image_tag}"
+#
+#
+#
+
+printf "%s\n" "${messages[@]}"
