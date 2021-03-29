c26d394ff57025b0c13f57fba652e850b6ce4a44
Davide Principi <davide.principi@nethesis.it>
2021-03-29 19:02:20 +0200

    Initial multi-dc setup

--- nsdc/build-image.sh
+++ nsdc/build-image.sh
@@ -9,7 +9,7 @@ if ! buildah inspect --type image "${repobase}/${reponame}" &>/dev/null; then
     buildah run "${container}" -- bash <<EOF
 set -e
 apt-get update
-apt-get -y install samba winbind
+apt-get -y install samba winbind krb5-user iputils-ping
 apt-get clean
 find /var/lib/apt/lists/ -type f -delete
 EOF
--- nsdc/entrypoint.sh
+++ nsdc/entrypoint.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-if ! grep -q -F "${REALM}" /etc/samba/smb.conf; then
+set -e
+
+if grep -q -F "${REALM}" /etc/samba/smb.conf; then
+    : # pass
+elif [[ $1 == "join-domain" ]]; then
+    echo "[NOTICE] Joining domain ${REALM}..."
+    rm -f /etc/samba/smb.conf
+    kinit ${ADMINUSER:-administrator} <<<"${ADMINPASS}"
+    samba-tool domain join ${REALM,,} DC \
+        -k yes \
+        "--option=dns forwarder = 127.0.0.53" \
+        "--option=bind interfaces only = yes" \
+        "--option=interfaces = 127.0.0.1 ${IPADDRESS}"
+    kdestroy
+elif [[ $1 == "new-domain" ]]; then
     echo "[NOTICE] Starting domain provisioning procedure..."
     rm -f /etc/samba/smb.conf
     samba-tool domain provision \
         --server-role=dc "--domain=${NBDOMAIN}" "--realm=${REALM}" \
         "--adminpass=${ADMINPASS:-Nethesis,1234}" \
         "--host-ip=${IPADDRESS}" \
+        "--option=dns forwarder = 127.0.0.53" \
         "--option=bind interfaces only = yes" \
         "--option=interfaces = 127.0.0.1 ${IPADDRESS}"
-    cp -av /var/lib/samba/private/krb5.conf /etc/krb5.conf
+else
+    echo "[NOTICE] arg $1 not recognized: now going to sleep +INF"
+    exec sleep +INF
 fi
 
 exec samba -i --debug-stderr
