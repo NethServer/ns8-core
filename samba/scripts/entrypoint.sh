a6becd37f0817fd566af58362ee37efe7579fe47
Davide Principi <davide.principi@nethesis.it>
2021-04-23 19:39:46 +0200

    Samba rootfull module refactor

--- samba/scripts/entrypoint.sh
+++ samba/scripts/entrypoint.sh
@@ -2,22 +2,24 @@
 
 set -e
 
-if grep -q -F "${REALM}" /etc/samba/smb.conf; then
-    : # pass
-elif [[ $1 == "join-domain" ]]; then
+provision_type=${1:?The mandatory entrypoint argument is missing}
+
+if [[ $provision_type == "run-dc" ]]; then
+    exec samba -i --debug-stderr
+elif [[ $provision_type == "join-domain" ]]; then
     echo "[NOTICE] Joining domain ${REALM}..."
     rm -f /etc/samba/smb.conf
     kinit ${ADMINUSER:-administrator} <<<"${ADMINPASS}"
-    samba-tool domain join ${REALM,,} DC \
+    exec samba-tool domain join ${REALM,,} DC \
         -k yes \
         "--option=dns forwarder = 127.0.0.53" \
         "--option=bind interfaces only = yes" \
         "--option=interfaces = 127.0.0.1 ${IPADDRESS}"
     kdestroy
-elif [[ $1 == "new-domain" ]]; then
+elif [[ $provision_type == "new-domain" ]]; then
     echo "[NOTICE] Starting domain provisioning procedure..."
     rm -f /etc/samba/smb.conf
-    samba-tool domain provision \
+    exec samba-tool domain provision \
         --server-role=dc "--domain=${NBDOMAIN}" "--realm=${REALM}" \
         "--adminpass=${ADMINPASS:-Nethesis,1234}" \
         "--host-ip=${IPADDRESS}" \
@@ -25,8 +27,6 @@ elif [[ $1 == "new-domain" ]]; then
         "--option=bind interfaces only = yes" \
         "--option=interfaces = 127.0.0.1 ${IPADDRESS}"
 else
-    echo "[NOTICE] arg $1 not recognized: now going to sleep +INF"
+    echo "[WARNING] Provision type $provision_type is not recognized: now going to sleep +INF"
     exec sleep +INF
 fi
-
-exec samba -i --debug-stderr
