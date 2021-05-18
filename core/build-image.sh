8acfbecd04afffc477e6b6d835554196db1bb656
Davide Principi <davide.principi@nethesis.it>
2021-05-18 18:21:36 +0200

    Add GitHub workflow for core and traefik

--- core/build-image.sh
+++ core/build-image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -e
-messages=("Publish the images with:" "")
-repobase="ghcr.io/nethserver"
+images=()
+repobase="${REPOBASE:-ghcr.io/nethserver}"
 
 # Reuse existing gobuilder-core container, to speed up builds
 if ! buildah containers --format "{{.ContainerName}}" | grep -q gobuilder-core; then
     echo "Pulling Golang runtime..."
-    buildah from --name gobuilder-core -v ${PWD}:/usr/src/core:Z docker.io/library/golang:1.16-alpine
+    buildah from --name gobuilder-core -v "${PWD}:/usr/src/core:Z" docker.io/library/golang:1.16-alpine
 fi
 
 echo "Build statically linked Go binaries based on Musl..."
@@ -19,13 +19,13 @@ buildah run gobuilder-core sh -c "cd /usr/src/core/api-server && CGO_ENABLED=0 g
 echo "Building the Core image..."
 container=$(buildah from scratch)
 reponame="core"
-buildah add ${container} imageroot /
-buildah add ${container} agent/agent /usr/local/bin/agent
-buildah add ${container} api-server/api-server /usr/local/bin/api-server
-buildah config --entrypoint=/ ${container}
+buildah add "${container}" imageroot /
+buildah add "${container}" agent/agent /usr/local/bin/agent
+buildah add "${container}" api-server/api-server /usr/local/bin/api-server
+buildah config --entrypoint=/ "${container}"
 buildah commit "${container}" "${repobase}/${reponame}"
-buildah rm ${container}
-messages+=(" buildah push ${repobase}/${reponame} docker://${repobase}/${reponame}:latest")
+buildah rm "${container}"
+images+=("${repobase}/${reponame}")
 
 echo "Building the Redis image..."
 container=$(buildah from docker.io/redis:6-alpine)
@@ -54,9 +54,16 @@ masterauth nopass
 EOR
 
 EOF
-buildah config --volume=/data '--cmd=[ "redis-server", "/data/etc/redis.conf" ]' ${container}
+buildah config --volume=/data '--cmd=[ "redis-server", "/data/etc/redis.conf" ]' "${container}"
 buildah commit "${container}" "${repobase}/${reponame}"
-buildah rm ${container}
-messages+=(" buildah push ${repobase}/${reponame} docker://${repobase}/${reponame}:latest")
+buildah rm "${container}"
+images+=("${repobase}/${reponame}")
 
-printf "%s\n" "${messages[@]}"
+if [[ -n "${CI}" ]]; then
+    # Set output value for Github Actions
+    printf "::set-output name=images::%s\n" "${images[*]}"
+else
+    printf "Publish the images with:\n\n"
+    for image in "${images[@]}"; do printf "  buildah push %s docker://%s:latest\n" "${image}" "${image}" ; done
+    printf "\n"
+fi
