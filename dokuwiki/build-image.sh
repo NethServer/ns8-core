73d4f6ce557feaafcb4ec1e3555b7b050a712368
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2021-05-19 12:23:00 +0200

    dokuwiki buildimage: documentation and add CI

--- dokuwiki/build-image.sh
+++ dokuwiki/build-image.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
+# Terminate on error
 set -e
-messages=("Publish the image with:" "")
-repobase="ghcr.io/nethserver"
 
+# Prepare variables for later user
+images=()
+# The image willbe pushed to GitHub image registry under nethserver organization
+repobase="ghcr.io/nethserver"
+# Configure the image name
 reponame="dokuwiki"
+
+# Create a new empty container image
 container=$(buildah from scratch)
 
+# Add imageroot directory to the container image
 buildah add "${container}" imageroot /
+# Setup the entrypoint and ask to reserve one TCP port with the label
 buildah config --entrypoint=/ --label="org.nethserver/tcp_ports_demand=1" "${container}"
+# Commit everything
 buildah commit "${container}" "${repobase}/${reponame}"
-messages+=(" buildah push ${repobase}/${reponame} docker://${repobase}/${reponame}:latest")
 
-#
-#
-#
+images+=("${repobase}/${reponame}")
 
-printf "%s\n" "${messages[@]}"
+# Setup CI when pushing to Github
+if [[ -n "${CI}" ]]; then
+    # Set output value for Github Actions
+    printf "::set-output name=images::%s\n" "${images[*]}"
+else
+    # Just print info for manual push
+    printf "Publish the images with:\n\n"
+    for image in "${images[@]}"; do printf "  buildah push %s docker://%s:latest\n" "${image}" "${image}" ; done
+    printf "\n"
+fi
