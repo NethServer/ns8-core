2cd53746240d6b485019ab023aab070b98163465
Davide Principi <davide.principi@nethesis.it>
2021-05-25 09:29:26 +0200

    ldapproxy. Configure GitHub actions workflow

--- ldapproxy/build-image.sh
+++ ldapproxy/build-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-messages=("Publish the image with:" "")
+images=()
 repobase="ghcr.io/nethserver"
 
 reponame="ldapproxy"
@@ -10,10 +10,17 @@ container=$(buildah from scratch)
 buildah add "${container}" imageroot /
 buildah config --entrypoint=/ "${container}"
 buildah commit "${container}" "${repobase}/${reponame}"
-messages+=(" buildah push ${repobase}/${reponame} docker://${repobase}/${reponame}:latest")
+images+=("${repobase}/${reponame}")
 
 #
 #
 #
 
-printf "%s\n" "${messages[@]}"
+if [[ -n "${CI}" ]]; then
+    # Set output value for Github Actions
+    printf "::set-output name=images::%s\n" "${images[*]}"
+else
+    printf "Publish the images with:\n\n"
+    for image in "${images[@]}"; do printf "  buildah push %s docker://%s:latest\n" "${image}" "${image}" ; done
+    printf "\n"
+fi
