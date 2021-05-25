a0645fd64b63b0b992a8d1d2f9662cf271f7598c
Andrea Leardini <andre8244@gmail.com>
2021-05-25 15:00:03 +0200

    ui: support for port in URL

--- core/ui/public/config/config.production.js
+++ core/ui/public/config/config.production.js
@@ -2,8 +2,16 @@ window.CONFIG = {
   PRODUCT_NAME: "NethServer 8",
   DOCS_URL: "https://www.nethserver.org/documentation/",
   COMPANY_NAME: "Nethesis",
-  API_ENDPOINT: window.location.hostname + window.location.pathname + "api",
+  API_ENDPOINT:
+    window.location.hostname +
+    (window.location.port ? ":" + window.location.port : "") +
+    window.location.pathname +
+    "api",
   API_SCHEME: window.location.protocol + "//",
   WS_ENDPOINT:
-    "wss://" + window.location.hostname + window.location.pathname + "ws",
+    "wss://" +
+    window.location.hostname +
+    (window.location.port ? ":" + window.location.port : "") +
+    window.location.pathname +
+    "ws",
 };
