ba7c6bfc1717e0d5cc0c19834dfa5c638bbff69e
Matteo Valentini <matteo.valentini@nethesis.it>
2021-07-14 14:48:20 +0200

    core: logcli wrapper, use the default credentials

--- core/imageroot/var/lib/nethserver/node/logcli.sh
+++ core/imageroot/var/lib/nethserver/node/logcli.sh
@@ -7,6 +7,8 @@
 if [[ -z "${LOKI_ADDR}" ]]; then
 	loki_id=$(REDIS_USER=default redis-exec GET cluster/default_instance/loki)
 	export LOKI_ADDR="http://$(REDIS_USER=default redis-exec HGET module/${loki_id}/environment LOKI_ADDR):3100"
+	export LOKI_USERNAME=$(REDIS_USER=default redis-exec HGET module/${loki_id}/environment LOKI_API_AUTH_USERNAME)
+	export LOKI_PASSWORD=$(REDIS_USER=default redis-exec HGET module/${loki_id}/environment LOKI_API_AUTH_PASSWORD)
 fi
 
 /usr/local/sbin/logcli "${@}"
