12039f5d2617b3a96a7cbfd50bb6c66ddf714964
Matteo Valentini <matteo.valentini@nethesis.it>
2021-07-15 17:24:33 +0200

    core & promtail: configure loki instance http port

--- core/imageroot/var/lib/nethserver/node/logcli.sh
+++ core/imageroot/var/lib/nethserver/node/logcli.sh
@@ -6,7 +6,8 @@
 
 if [[ -z "${LOKI_ADDR}" ]]; then
 	loki_id=$(REDIS_USER=default redis-exec GET cluster/default_instance/loki)
-	export LOKI_ADDR="http://$(REDIS_USER=default redis-exec HGET module/${loki_id}/environment LOKI_ADDR):3100"
+	loki_http_port=$(REDIS_USER=default redis-exec HGET module/${loki_id}/environment LOKI_HTTP_PORT)
+	export LOKI_ADDR="http://$(REDIS_USER=default redis-exec HGET module/${loki_id}/environment LOKI_ADDR):${loki_http_port}"
 	export LOKI_USERNAME=$(REDIS_USER=default redis-exec HGET module/${loki_id}/environment LOKI_API_AUTH_USERNAME)
 	export LOKI_PASSWORD=$(REDIS_USER=default redis-exec HGET module/${loki_id}/environment LOKI_API_AUTH_PASSWORD)
 fi
