24864471f245e412388fc310287ee97c2284b480
Andrea Leardini <andre8244@gmail.com>
2021-09-16 16:43:15 +0200

    ui. fix import path of ns8.css in external apps

--- core/ui/public/config/config.development.js
+++ core/ui/public/config/config.development.js
@@ -3,11 +3,13 @@ window.CONFIG = {
   DOCS_URL: "https://www.nethserver.org/documentation/",
   COMPANY_NAME: "Nethesis",
   // API_ENDPOINT: window.location.hostname + ":8081/api", ////
-  API_ENDPOINT: "172.25.5.229/cluster-admin/api", //// leader node
+  // API_ENDPOINT: "172.25.5.229/cluster-admin/api", //// leader node
   // API_ENDPOINT: "172.25.5.3/cluster-admin/api", //// worker node
+  API_ENDPOINT: "192.168.122.220/cluster-admin/api", //// leader node (local)
   // API_SCHEME: window.location.protocol + "//", ////
   API_SCHEME: "https://",
   // WS_ENDPOINT: "ws://" + window.location.hostname + ":8081/ws", ////
-  WS_ENDPOINT: "wss://172.25.5.229/cluster-admin/ws", //// leader node
+  // WS_ENDPOINT: "wss://172.25.5.229/cluster-admin/ws", //// leader node
   // WS_ENDPOINT: "wss://172.25.5.3/cluster-admin/ws", //// worker node
+  WS_ENDPOINT: "wss://192.168.122.220/cluster-admin/ws", //// leader node (local)
 };
