83c41bcc7001c20c6ea8bd92fda7d29b3092e8b7
Davide Principi <davide.principi@nethesis.it>
2021-09-27 16:30:16 +0200

    tasks. Reconnect both API and Redis clients
    Handle request retries and reconnect in the following scenarios
    
    - redis restart
    - api-server restart
    - traefik restart
    - network issues
    
    The clients do not handle task timeouts: a task is always expected to
    write the completion status in Redis. A task liveness check could be
    added for this purpose.

---
+++ core/imageroot/usr/local/nethserver/agent/python/agent/tasks/apiclient.py
@@ -0,0 +1,276 @@
+#
+# Copyright (C) 2021 Nethesis S.r.l.
+# http://www.nethesis.it - nethserver@nethesis.it
+#
+# This script is part of NethServer.
+#
+# NethServer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# NethServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NethServer.  If not, see COPYING.
+#
+
+import aiohttp
+import asyncio
+import random
+import json
+import os
+import urllib
+import sys
+from .exceptions import *
+
+async def run_apiclient_nowait(taskrq, **kwargs):
+    theaders = _get_theaders_cache(kwargs.pop('auth_token', None))
+    async with aiohttp.ClientSession(raise_for_status=True) as client:
+        return await _retry_request(_apost_task, taskrq, client=client, theaders=theaders, **kwargs)
+
+async def run_apiclient(taskrq, **kwargs):
+    theaders = _get_theaders_cache(kwargs.pop('auth_token', None))
+    async with aiohttp.ClientSession(raise_for_status=True) as client:
+        taskctx = {'status_path': None, 'rq': taskrq, 'lock': asyncio.Lock()}
+        # Context is initially locked. Unlock occurs when task has been
+        # submitted by _acontrol_task:
+        await taskctx['lock'].acquire()
+
+        tcontroller = asyncio.create_task(_retry_request(_acontrol_task, taskctx, client=client, theaders=theaders, **kwargs), name='tcontroller')
+        tpoll = asyncio.create_task(_retry_request(_apoll_status, taskctx, client=client, theaders=theaders, **kwargs), name = 'tpoll')
+
+        done, pending = await asyncio.wait({tcontroller, tpoll}, return_when=asyncio.FIRST_COMPLETED)
+        if tpoll in done:
+            tcontroller.cancel()
+            return tpoll.result()
+
+        if tcontroller in done:
+            tpoll.cancel()
+            return tcontroller.result()
+
+
+async def _alogin(**kwargs):
+    """Read the Redis user credentials from the environment and retrieve a new authorization token.
+    """
+    client = kwargs['client']
+    tls_verify = kwargs['tls_verify']
+    endpoint = kwargs['endpoint']
+    redis_username = os.environ['REDIS_USER'] # Fatal if missing!
+    redis_password = os.environ['REDIS_PASSWORD'] # Fatal if missing!
+    async with client.post(
+        f'{endpoint}/api/login',
+        json={
+            'username':redis_username,
+            'password':redis_password
+        },
+        ssl=tls_verify,
+    ) as resp:
+        jresp = await resp.json()
+
+    if 'AGENT_STATE_DIR' in os.environ:
+        cachefile = os.environ['AGENT_STATE_DIR'] + '/apitoken.cache'
+        oldmask = os.umask(0o77)
+        with open(cachefile, 'w') as fo:
+            fo.write(jresp['token'])
+        os.umask(oldmask)
+
+    return {"Authorization": "Bearer " + jresp['token']}
+
+async def _aread_status(path, **kwargs):
+    """Read the status of the given task_id from agent_id
+    """
+    client = kwargs['client']
+    theaders = kwargs['theaders']
+    tls_verify = kwargs['tls_verify']
+    endpoint = kwargs['endpoint']
+
+    async with client.get(
+        f'{endpoint}/api/{path}/status',
+        ssl=tls_verify,
+        headers=theaders,
+    ) as resp:
+        jresp = await resp.json()
+
+    return {
+        'output': jresp['data']['output'],
+        'error': jresp['data']['error'],
+        'exit_code': jresp['data']['exit_code'],
+    }
+
+def _get_theaders_cache(token=None):
+    """Build authorization headers from the file cache contents.
+    """
+    if token:
+        return {
+            "Authorization": "Bearer " + token
+        }
+
+    if 'AGENT_STATE_DIR' in os.environ:
+        cachefile = os.environ['AGENT_STATE_DIR'] + '/apitoken.cache'
+    else:
+        cachefile = None
+
+    theaders = {}
+    try:
+        with open(cachefile) as f:
+            token = f.read().strip()
+            theaders["Authorization"] = "Bearer " + token
+    except:
+        pass
+
+    return theaders
+
+async def _retry_request(request_procedure, *args, **kwargs):
+    """Handle request retries and authentication issues"""
+    retry_delay = kwargs.pop('retry_initdelay', 1.0)
+    retry_delay += random.uniform(0.0, retry_delay)
+    attempts = kwargs.pop('retry_attempts', 20)
+    incfactor = kwargs.pop('retry_incfactor', 2.0)
+    kwargs.setdefault('retry_sendlogin', True)
+    http_temporary_errors = [500, 502, 503, 504]
+    http_temporary_errors += kwargs.pop('retry_statuscodes', [])
+
+    request_name = request_procedure.__name__
+    last_retry_error = ''
+    for attempt in range(1, attempts+1):
+        try:
+            #
+            # request_procedure invocation
+            #
+            retval = await request_procedure(*args, **kwargs)
+            if attempt > 1:
+                print(f'{request_name} request recovered successfully at attempt {attempt}', file=sys.stderr)
+            return retval
+
+        except TaskRetryWsDisconnect as extask:
+            retry_error = str(extask)
+        except aiohttp.WSServerHandshakeError as exws:
+            if exws.status == 200 and kwargs['retry_sendlogin'] is True:
+                break # for loop
+            else:
+                raise exws
+        except aiohttp.ClientConnectorError as exhttp:
+            retry_error = str(exhttp)
+        except aiohttp.ClientResponseError as exhttp:
+            if exhttp.status in http_temporary_errors:
+                retry_error = str(exhttp)
+            elif exhttp.status == 401 and kwargs['retry_sendlogin'] is True:
+                break # for loop. Send a login request before resuming "request_procedure"
+            else:
+                if attempt > 1:
+                    frameback = sys.exc_info()[2].tb_frame.f_back
+                    print(f'{request_name} request from {frameback} discarded due to {exhttp} exception at attempt {attempt}', file=sys.stderr)
+                raise exhttp
+        except Exception as ex:
+            if attempt > 1:
+                frameback = sys.exc_info()[2].tb_frame.f_back
+                print(f'{request_name} request from {frameback} discarded due to {ex} at attempt {attempt}', file=sys.stderr)
+            raise
+
+        await asyncio.sleep(retry_delay)
+        retry_delay *= incfactor
+        if retry_error != last_retry_error:
+            print(f'{request_name} request attempt failed ({retry_error}). Retrying...', file=sys.stderr)
+            last_retry_error = retry_error
+    else:
+        raise TaskRetryAbort(f'{request_name} request failed at attempt {attempt}')
+
+    #
+    # If the request failed due to a bad auth, retry immediately starting
+    # with a login request. Recursion is blocked by the
+    # retry_sendlogin=False flag.
+    #
+    kwargs.pop('retry_sendlogin')
+    kwargs.pop('theaders')
+    theaders = await _retry_request(_alogin, retry_sendlogin=False, **kwargs)
+    return await _retry_request(request_procedure, *args, theaders=theaders, retry_sendlogin=False, **kwargs)
+
+
+async def _apost_task(taskrq, **kwargs):
+    """Create a new task for the given agent_id and return its task_id.
+    """
+    client = kwargs['client']
+    theaders = kwargs['theaders']
+    tls_verify = kwargs['tls_verify']
+    endpoint = kwargs['endpoint']
+    agent_id = taskrq['agent_id']
+
+    async with client.post(
+        f'{endpoint}/api/{agent_id}/tasks',
+        json={
+            'action': taskrq['action'],
+            'data': taskrq['data'],
+            'parent': taskrq['parent'],
+        },
+        ssl=tls_verify,
+        headers=theaders,
+    ) as resp:
+
+        jresp = await resp.json()
+
+    return agent_id + '/task/' + jresp['data']['id']
+
+def _get_token(theaders):
+    if not 'Authorization' in theaders:
+        return ''
+    return theaders['Authorization'][len('Bearer '):]
+
+async def _acontrol_task(taskctx, **kwargs):
+
+    client = kwargs['client']
+    theaders = kwargs['theaders']
+    tls_verify = kwargs['tls_verify']
+    endpoint = kwargs['endpoint']
+    progress_callback = kwargs['progress_callback']
+    # Connect the web socket before the task is submitted to catch the
+    # relevant progress messages
+    async with client.ws_connect(
+        f'{endpoint}/ws?jwt=' + urllib.parse.quote_plus(_get_token(theaders)),
+        ssl=tls_verify,
+        heartbeat=4.0,
+    ) as ws:
+        # Check the context to submit the task only the first time
+        if taskctx['lock'].locked() and taskctx['status_path'] is None:
+            taskctx['status_path'] = await _retry_request(_apost_task, taskctx['rq'], **kwargs)
+            taskctx['lock'].release() # _apoll_status is waiting on the lock
+        async for msg in ws:
+            if msg.type != aiohttp.WSMsgType.TEXT:
+                continue
+
+            try:
+                jdata=json.loads(msg.data)
+            except:
+                continue # Discard unknown message
+
+            if jdata['name'] == 'progress/' + taskctx['status_path']:
+                if progress_callback:
+                    progress_callback(jdata['payload']['progress'])
+                if jdata['payload']['status'] in ['completed', 'aborted', 'validation-failed']:
+                    break # for
+        else:
+            raise TaskRetryWsDisconnect(f"WS reached EOF while waiting for {taskctx['status_path']}")
+
+    return await _retry_request(_aread_status, taskctx['status_path'], retry_statuscodes=[400,404], **kwargs)
+
+async def _apoll_status(taskctx, **kwargs):
+    """Read in a loop the status of the given task_id from agent_id
+    """
+    client = kwargs['client']
+    theaders = kwargs['theaders']
+    tls_verify = kwargs['tls_verify']
+    endpoint = kwargs['endpoint']
+    with_sleep = False
+
+    while True:
+        async with taskctx['lock']: # wait until the context is unlocked by _acontrol_task
+            try:
+                return await _aread_status(taskctx['status_path'], **kwargs)
+            except aiohttp.ClientResponseError as exhttp:
+                if not exhttp.status in [400,404]:
+                    raise
+
+        await asyncio.sleep(5.0)
---
+++ core/imageroot/usr/local/nethserver/agent/python/agent/tasks/redisclient.py
@@ -0,0 +1,187 @@
+#
+# Copyright (C) 2021 Nethesis S.r.l.
+# http://www.nethesis.it - nethserver@nethesis.it
+#
+# This script is part of NethServer.
+#
+# NethServer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# NethServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NethServer.  If not, see COPYING.
+#
+
+import aioredis
+import asyncio
+import json
+import os
+import sys
+import uuid
+import random
+from .exceptions import *
+
+async def run_redisclient_nowait(taskrq, **kwargs):
+    redis_username = os.environ['REDIS_USER'] # Fatal if missing!
+    redis_password = os.environ['REDIS_PASSWORD'] # Fatal if missing!
+    async with aioredis.from_url(
+        kwargs['endpoint'],
+        username=redis_username,
+        password=redis_password,
+        decode_responses=True
+    ) as rdb:
+        task_id = str(uuid.uuid4())
+        task_obj = {
+            "id": task_id,
+            "action": taskrq['action'],
+            "data": taskrq['data'],
+            "parent": taskrq['parent'],
+        }
+        await _retry_request(rdb.lpush, f'{taskrq["agent_id"]}/tasks', json.dumps(task_obj))
+
+    return taskrq["agent_id"] + '/task/' + task_id
+
+async def run_redisclient(taskrq, **kwargs):
+    redis_username = os.environ['REDIS_USER'] # Fatal if missing!
+    redis_password = os.environ['REDIS_PASSWORD'] # Fatal if missing!
+    async with aioredis.from_url(
+        kwargs['endpoint'],
+        username=redis_username,
+        password=redis_password,
+        decode_responses=True,
+    ) as rdb:
+        task_id = str(uuid.uuid4())
+        taskctx = {
+            'id': task_id,
+            'status_path': taskrq['agent_id'] + '/task/' + task_id,
+            'rq': taskrq,
+            'lock': asyncio.Lock(),
+            'pushed': False,
+        }
+        # Context is initially locked. Unlock occurs when task has been
+        # submitted by _acontrol_task:
+        await taskctx['lock'].acquire()
+
+        tcontrol = asyncio.create_task(_retry_request(_acontrol_task, rdb, taskctx, **kwargs), name='tcontrol')
+        tpoll = asyncio.create_task(_retry_request(_apoll_status, rdb, taskctx), name='tpoll')
+
+        done, pending = await asyncio.wait({tcontrol, tpoll}, return_when=asyncio.FIRST_COMPLETED)
+
+        if tpoll in done:
+            tcontrol.cancel()
+            return tpoll.result()
+
+        if tcontrol in done:
+            tpoll.cancel()
+            return tcontrol.result()
+
+async def _apoll_status(rdb, taskctx):
+    while True:
+        async with taskctx['lock']:
+            try:
+                return await _aread_status(rdb, taskctx['status_path'])
+            except TaskStatusNotFound:
+                pass
+
+        await asyncio.sleep(5.0)
+
+async def _aread_status(rdb, status_path):
+
+    output, error, exit_code = await rdb.mget(
+        status_path + '/output',
+        status_path + '/error',
+        status_path + '/exit_code',
+    )
+
+    if output is None or exit_code is None or error is None:
+        raise TaskStatusNotFound('Task status %s not found' % status_path)
+
+    try:
+        output = json.loads(output) # Attempt to decode output as a JSON string
+    except:
+        pass
+
+    if exit_code.isnumeric():
+        exit_code = int(exit_code)
+
+    return {
+        'output': output,
+        'error': error,
+        'exit_code': exit_code,
+    }
+
+async def _acontrol_task(rdb, taskctx, **kwargs):
+    progress_callback = kwargs['progress_callback']
+    async with rdb.pubsub() as pubsub:
+        await _retry_request(pubsub.subscribe, 'progress/' + taskctx['status_path'])
+
+        if taskctx['lock'].locked() and taskctx['pushed'] is False:
+            taskrq = taskctx['rq']
+            task_obj = {
+                "id": taskctx['id'],
+                "action": taskrq["action"],
+                "data": taskrq["data"],
+                "parent": taskrq["parent"],
+            }
+            await _retry_request(rdb.lpush, taskrq['agent_id'] + '/tasks', json.dumps(task_obj))
+            taskctx['pushed'] = True
+            taskctx['lock'].release() # _apoll_status is waiting on the lock
+
+        async for message in pubsub.listen():
+            try:
+                jdata = json.loads(message['data'])
+            except:
+                continue # Discard unreadable message
+
+            if progress_callback:
+                progress_callback(jdata['progress'])
+
+            if jdata['status'] in ['completed', 'aborted', 'validation-failed']:
+                break
+        else:
+            raise TaskRetryPubSubDisconnect()
+
+        return await _retry_request(_aread_status, rdb, taskctx['status_path'])
+
+async def _retry_request(request_procedure, *args, **kwargs):
+    """Handle request retries and authentication issues"""
+    retry_delay = kwargs.pop('retry_initdelay', 1.0)
+    retry_delay += random.uniform(0.0, retry_delay)
+    attempts = kwargs.pop('retry_attempts', 20)
+    incfactor = kwargs.pop('retry_incfactor', 1.2)
+
+    request_name = request_procedure.__name__
+    last_retry_error = ''
+    for attempt in range(1, attempts+1):
+        try:
+            #
+            # request_procedure invocation
+            #
+            retval = await request_procedure(*args, **kwargs)
+            if attempt > 1:
+                print(f'{request_name} request recovered successfully at attempt {attempt}', file=sys.stderr)
+            return retval
+        except aioredis.ConnectionError as exredis:
+            retry_error = str(exredis)
+        except TaskRetryPubSubDisconnect as extask:
+            retry_error = str(exredis)
+        except Exception as ex:
+            if attempt > 1:
+                frameback = sys.exc_info()[2].tb_frame.f_back
+                print(f'{request_name} request from {frameback} discarded due to {ex} at attempt {attempt}', file=sys.stderr)
+            raise
+
+        await asyncio.sleep(retry_delay)
+        retry_delay *= incfactor
+        if retry_error != last_retry_error:
+            print(f'{request_name} request attempt failed ({retry_error}). Retrying...', file=sys.stderr)
+            last_retry_error = retry_error
+
+    else:
+        raise TaskRetryAbort(f'{request_name} request failed at attempt {attempt}')
