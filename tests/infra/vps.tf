ccf27df4222afbeb078b3c7b9673b5feb2f9c04a
Matteo Valentini <matteo.valentini@nethesis.it>
2021-11-04 12:40:16 +0100

    tests: add workpace in the instances's name

--- tests/infra/vps.tf
+++ tests/infra/vps.tf
@@ -11,7 +11,7 @@ variable "images" {
 resource "digitalocean_droplet" "vps" {
   for_each           = var.nodes
   image              = var.images[substr(each.key, 0, 2)]
-  name               = format("%s.%s", each.key, var.domain)
+  name               = format("%s.%s.%s", each.key, terraform.workspace ,var.domain)
   region             = each.value
   size               = "s-1vcpu-1gb-intel"
   ipv6               = substr(each.key, 0, 2) != "dn"
@@ -31,7 +31,7 @@ resource "digitalocean_record" "vps_ipv4" {
   type     = "A"
   domain   = data.digitalocean_domain.default.name
   value    = digitalocean_droplet.vps[each.key].ipv4_address
-  name     = each.key
+  name     = format("%s.%s", each.key, terraform.workspace)
   ttl      = 300
 }
 
