c80ffba83141567d77c9ab79303da667e041ac56
Stephane de Labrusse <stephdl@de-labrusse.fr>
2021-11-30 16:27:02 +0100

    UI. settings

--- mariadb/ui/src/views/Settings.vue
+++ mariadb/ui/src/views/Settings.vue
@@ -5,8 +5,7 @@
         <h2>{{ $t("settings.title") }}</h2>
       </div>
     </div>
-    <!-- sample settings page -->
-    <!-- <div v-if="error.getConfiguration" class="bx--row">
+    <div v-if="error.getConfiguration" class="bx--row">
       <div class="bx--col">
         <NsInlineNotification
           kind="error"
@@ -19,9 +18,55 @@
     <div class="bx--row">
       <div class="bx--col-lg-16">
         <cv-tile :light="true">
-          <cv-form @submit.prevent="saveSettings"> -->
-    <!-- TODO configuration fields -->
-    <!-- <div v-if="error.configureModule" class="bx--row">
+          <cv-form @submit.prevent="saveSettings">
+            <cv-text-input
+              :label="$t('settings.phpmyadmin_fqdn')"
+              placeholder="phpmyadmin.example.org"
+              v-model.trim="host"
+              class="mg-bottom"
+              :invalid-message="$t(error.host)"
+              :disabled="loading.settings"
+              ref="host"
+            >
+            </cv-text-input>
+            <cv-text-input
+              :label="$t('settings.mariadb_tcp_port')"
+              v-model.trim="mariadb_tcp_port"
+              class="mg-bottom"
+              :invalid-message="$t(error.mariadb_tcp_port)"
+              disabled
+              ref="host"
+            >
+            </cv-text-input>
+            <cv-toggle
+              value="letsEncrypt"
+              :label="$t('settings.lets_encrypt')"
+              v-model="isLetsEncryptEnabled"
+              :disabled="loading.settings"
+              class="mg-bottom"
+            >
+              <template slot="text-left">{{
+                $t("settings.disabled")
+              }}</template>
+              <template slot="text-right">{{
+                $t("settings.enabled")
+              }}</template>
+            </cv-toggle>
+            <cv-toggle
+              value="httpToHttps"
+              :label="$t('settings.http_to_https')"
+              v-model="isHttpToHttpsEnabled"
+              :disabled="loading.settings"
+              class="mg-bottom"
+            >
+              <template slot="text-left">{{
+                $t("settings.disabled")
+              }}</template>
+              <template slot="text-right">{{
+                $t("settings.enabled")
+              }}</template>
+            </cv-toggle>
+            <div v-if="error.configureModule" class="bx--row">
               <div class="bx--col">
                 <NsInlineNotification
                   kind="error"
@@ -41,12 +86,12 @@
           </cv-form>
         </cv-tile>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
-// import to from "await-to-js";
+import to from "await-to-js";
 import { mapState } from "vuex";
 import {
   QueryParamService,
@@ -67,12 +112,19 @@ export default {
         page: "settings",
       },
       urlCheckInterval: null,
+      host: "",
+      mariadb_tcp_port: "",
+      isLetsEncryptEnabled: false,
+      isHttpToHttpsEnabled: false,
       loading: {
         settings: true,
       },
       error: {
         getConfiguration: "",
         configureModule: "",
+        host: "",
+        lets_encrypt: "",
+        http2https: "",
       },
     };
   },
@@ -80,7 +132,7 @@ export default {
     ...mapState(["instanceName", "ns8Core", "appName"]),
   },
   created() {
-    // this.getConfiguration();
+    this.getConfiguration();
   },
   beforeRouteEnter(to, from, next) {
     next((vm) => {
@@ -93,100 +145,129 @@ export default {
     next();
   },
   methods: {
-    // async getConfiguration() {
-    //   this.loading.settings = true;
-    //   this.error.getConfiguration = "";
-    //   const taskAction = "get-configuration";
-    //   // register to task completion
-    //   this.ns8Core.$root.$once(
-    //     taskAction + "-completed",
-    //     this.getConfigurationCompleted
-    //   );
-    //   const res = await to(
-    //     this.createModuleTaskForApp(this.instanceName, {
-    //       action: taskAction,
-    //       extra: {
-    //         title: this.$t("action." + taskAction),
-    //         isNotificationHidden: true,
-    //       },
-    //     })
-    //   );
-    //   const err = res[0];
-    //   if (err) {
-    //     console.error(`error creating task ${taskAction}`, err);
-    //     this.error.getConfiguration = this.getErrorMessage(err);
-    //     return;
-    //   }
-    // },
-    // validateSaveSettings() {
-    //   this.clearErrors(this);
-    //   let isValidationOk = true;
-    //   // TODO validate settings
-    //   return isValidationOk;
-    // },
-    // saveSettingsValidationFailed(validationErrors) {
-    //   this.loading.settings = false;
-    //   for (const validationError of validationErrors) {
-    //     const param = validationError.parameter;
-    //     // set i18n error message
-    //     this.error[param] = "settings." + validationError.error;
-    //   }
-    // },
-    // async saveSettings() {
-    //   const isValidationOk = this.validateSaveSettings();
-    //   if (!isValidationOk) {
-    //     return;
-    //   }
-    //   this.loading.settings = true;
-    //   const taskAction = "configure-module";
-    //   // register to task validation
-    //   this.ns8Core.$root.$off(taskAction + "-validation-failed");
-    //   this.ns8Core.$root.$once(
-    //     taskAction + "-validation-failed",
-    //     this.saveSettingsValidationFailed
-    //   );
-    //   // register to task completion
-    //   this.ns8Core.$root.$off(taskAction + "-completed");
-    //   this.ns8Core.$root.$once(
-    //     taskAction + "-completed",
-    //     this.saveSettingsCompleted
-    //   );
-    //   const res = await to(
-    //     this.createModuleTaskForApp(this.instanceName, {
-    //       action: taskAction,
-    //       data: {
-    //         // TODO
-    //       },
-    //       extra: {
-    //         title: this.$t("settings.instance_configuration", {
-    //           instance: this.instanceName,
-    //         }),
-    //         description: this.$t("settings.configuring"),
-    //       },
-    //     })
-    //   );
-    //   const err = res[0];
-    //   if (err) {
-    //     console.error(`error creating task ${taskAction}`, err);
-    //     this.error.configureModule = this.getErrorMessage(err);
-    //     this.loading.settings = false;
-    //     return;
-    //   }
-    // },
-    // getConfigurationCompleted(taskContext, taskResult) {
-    //   const config = taskResult.output;
-    //   // TODO
-    //   this.focusElement("wikiName");
-    // },
-    // saveSettingsCompleted() {
-    //   this.loading.settings = false;
-    //   // reload configuration
-    //   this.getConfiguration();
-    // },
+    async getConfiguration() {
+      this.loading.settings = true;
+      this.error.getConfiguration = "";
+      const taskAction = "get-configuration";
+
+      // register to task completion
+      this.ns8Core.$root.$once(
+        taskAction + "-completed",
+        this.getConfigurationCompleted
+      );
+
+      const res = await to(
+        this.createModuleTaskForApp(this.instanceName, {
+          action: taskAction,
+          extra: {
+            title: this.$t("action." + taskAction),
+            isNotificationHidden: true,
+          },
+        })
+      );
+      const err = res[0];
+
+      if (err) {
+        console.error(`error creating task ${taskAction}`, err);
+        this.error.getConfiguration = this.getErrorMessage(err);
+        return;
+      }
+    },
+    validateSaveSettings() {
+      this.clearErrors(this);
+
+      let isValidationOk = true;
+
+      if (!this.host) {
+        this.error.host = "common.required";
+
+        if (isValidationOk) {
+          this.focusElement("host");
+        }
+        isValidationOk = false;
+      }
+
+      return isValidationOk;
+    },
+    saveSettingsValidationFailed(validationErrors) {
+      this.loading.settings = false;
+
+      for (const validationError of validationErrors) {
+        const param = validationError.parameter;
+        // set i18n error message
+        this.error[param] = "settings." + validationError.error;
+      }
+    },
+    async saveSettings() {
+      const isValidationOk = this.validateSaveSettings();
+      if (!isValidationOk) {
+        return;
+      }
+
+      this.loading.settings = true;
+      const taskAction = "configure-module";
+
+      // register to task validation
+      this.ns8Core.$root.$off(taskAction + "-validation-failed");
+      this.ns8Core.$root.$once(
+        taskAction + "-validation-failed",
+        this.saveSettingsValidationFailed
+      );
+
+      // register to task completion
+      this.ns8Core.$root.$off(taskAction + "-completed");
+      this.ns8Core.$root.$once(
+        taskAction + "-completed",
+        this.saveSettingsCompleted
+      );
+
+      const res = await to(
+        this.createModuleTaskForApp(this.instanceName, {
+          action: taskAction,
+          data: {
+            host: this.host,
+            lets_encrypt: this.isLetsEncryptEnabled,
+            http2https: this.isHttpToHttpsEnabled,
+          },
+          extra: {
+            title: this.$t("settings.instance_configuration", {
+              instance: this.instanceName,
+            }),
+            description: this.$t("settings.configuring"),
+          },
+        })
+      );
+      const err = res[0];
+
+      if (err) {
+        console.error(`error creating task ${taskAction}`, err);
+        this.error.configureModule = this.getErrorMessage(err);
+        this.loading.settings = false;
+        return;
+      }
+    },
+    getConfigurationCompleted(taskContext, taskResult) {
+      const config = taskResult.output;
+      this.host = config.host;
+      this.mariadb_tcp_port = config.mariadb_tcp_port;
+      this.isLetsEncryptEnabled = config.lets_encrypt;
+      this.isHttpToHttpsEnabled = config.http2https;
+      this.loading.settings = false;
+      this.focusElement("host");
+    },
+    saveSettingsCompleted() {
+      this.loading.settings = false;
+
+      // reload configuration
+      this.getConfiguration();
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
 @import "../styles/carbon-utils";
+.mg-bottom {
+  margin-bottom: $spacing-06;
+}
 </style>
