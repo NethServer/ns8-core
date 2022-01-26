1c38b834270fee2df01c14a2b64b00f1afd8ff33
Davide Principi <davide.principi@nethesis.it>
2022-01-26 18:07:52 +0100

    core. Fix AWS region configuration

--- core/imageroot/usr/local/agent/pypkg/agent/__init__.py
+++ core/imageroot/usr/local/agent/pypkg/agent/__init__.py
@@ -141,6 +141,8 @@ def run_restic(rdb, repository, repo_path, podman_args, restic_args, **kwargs):
         restic_env["RESTIC_REPOSITORY"] = orepo['url'] + "/" + repo_path
         restic_env["AWS_ACCESS_KEY"] = orepo['aws_access_key_id']
         restic_env["AWS_SECRET_ACCESS_KEY"] = orepo['aws_secret_access_key']
+        if orepo['provider'] == 'aws':
+            restic_env['AWS_DEFAULT_REGION'] = orepo.get('aws_default_region', '')
     elif uschema == 'b2':
         restic_env["RESTIC_REPOSITORY"] = orepo['url'] + ":" + repo_path
         restic_env["B2_ACCOUNT_ID"] = orepo['b2_account_id']
