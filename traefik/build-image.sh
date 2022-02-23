db246d78be83d7a9fc19d1d6bb5b137dda46fcec
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-02-23 17:17:07 +0100

    traefik: add org.nethserver.flags

--- traefik/build-image.sh
+++ traefik/build-image.sh
@@ -11,6 +11,7 @@ buildah add "${container}" imageroot /imageroot
 buildah add "${container}" ui /ui
 buildah config --entrypoint=/ \
     --label="org.nethserver.images=docker.io/traefik:v2.4" \
+    --label="org.nethserver.flags=core_module no_data_backup" \
     "${container}"
 buildah commit "${container}" "${repobase}/${reponame}"
 images+=("${repobase}/${reponame}")
