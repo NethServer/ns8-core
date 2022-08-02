6e39710396ce73a252cb7bd781a654c6be9914e9
Davide Principi <davide.principi@nethesis.it>
2022-08-02 17:16:26 +0200

    api-server. Match log records from sub-components
    Some modules (e.g. Mail) use distinct SYSLOG_IDENTIFIER strings, to
    distinguish the module component generating the log record. For
    instance: mail1/master, mail1, mail1/submission/smtpd ...
    
    Extend the match expression to allow an optional "/suffix" notation.
    
    Allow to search in the SYSLOG_IDENTIFIER content, too.

--- core/api-server/socket/action.go
+++ core/api-server/socket/action.go
@@ -117,7 +117,7 @@ func Action(socketAction models.SocketAction, s *melody.Session, wg *sync.WaitGr
 
 		// check filter
 		if len(logsAction.Filter) > 0 {
-			filter = "json |  line_format \"{{.SYSLOG_TIMESTAMP}} {{.nodename}} {{.MESSAGE}}\" |= `" + logsAction.Filter + "`"
+			filter = "json | line_format \"{{.SYSLOG_TIMESTAMP}} {{.SYSLOG_IDENTIFIER}} {{.nodename}} {{.MESSAGE}}\" |= `" + logsAction.Filter + "`"
 		} else {
 			filter = "json"
 		}
@@ -131,7 +131,7 @@ func Action(socketAction models.SocketAction, s *melody.Session, wg *sync.WaitGr
 			entity = "{nodename=\"" + logsAction.EntityName + "\"} | " + filter + " | line_format \"{{.nodename}} {{.MESSAGE}}\""
 
 		case "module":
-			entity = "{job=\"systemd-journal\"} | " + filter + " | SYSLOG_IDENTIFIER=\"" + logsAction.EntityName + "\" | line_format \"{{.nodename}} {{.MESSAGE}}\""
+			entity = "{job=\"systemd-journal\"} | " + filter + " | SYSLOG_IDENTIFIER=~\"" + logsAction.EntityName + "(/.+)?\" | line_format \"{{.SYSLOG_IDENTIFIER}} {{.MESSAGE}}\""
 
 		}
 		args = append(args, entity)
