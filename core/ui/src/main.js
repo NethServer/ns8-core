38d1bb19dc1bc882db4fedd0dc973d03dc11d8ce
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2023-02-21 09:49:14 +0100

    ui: i18n, refactor language load
    Each language has now its own sub-directory.
    This is a more standard approach which should ease weblate ingration.

--- core/ui/src/main.js
+++ core/ui/src/main.js
@@ -61,8 +61,8 @@ for (const f in Filters) {
 import VueI18n from "vue-i18n";
 Vue.use(VueI18n);
 const i18n = new VueI18n();
-const messages = require("../public/i18n/en.json");
 const langCode = navigator.language.substring(0, 2);
+const messages = require("../public/i18n/"+langCode+"/translation.json");
 i18n.setLocaleMessage(langCode, messages);
 i18n.locale = langCode;
 
