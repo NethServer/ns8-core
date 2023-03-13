c1f65d25a65275787ff9faafe873e0e492b33ff4
Matteo Valentini <matteo.valentini@nethesis.it>
2023-03-13 17:48:12 +0100

    Two steps installation process
    Separate the installation of an NS8 system into two steps, in the first
    step will be installed all the software needed and in the second step
    will be configured the system and will be created the passwords, keys,
    and tokens.
    
    All the software is installed by the `install-core.sh` and the
    `install-finalize.sh` script will configure the systems and generate the
    credentials.
    
    This will ease the creation of a generic image that can be used as a
    base for future parametrized installations.
    
    The feature controlled is by the environment variable
    `NS8_TWO_STEPS_INSTALL`

--- core/imageroot/var/lib/nethserver/node/install-core.sh
+++ core/imageroot/var/lib/nethserver/node/install-core.sh
@@ -33,15 +33,6 @@ source /etc/nethserver/core.env
 echo "Pulling rclone image ${RCLONE_IMAGE}:"
 podman pull "${RCLONE_IMAGE}"
 
-if [[ ! -f ~/.ssh/id_rsa.pub ]] ; then
-    echo "Generating a new RSA key pair for SSH:"
-    ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
-fi
-
-echo "Adding id_rsa.pub to module skeleton dir:"
-install -d -m 700 /etc/nethserver/skel/.ssh
-install -m 600 -T ~/.ssh/id_rsa.pub /etc/nethserver/skel/.ssh/authorized_keys
-
 if ! id "api-server" &>/dev/null; then
     echo "Create the api-server user:"
     useradd -r -m -d /var/lib/nethserver/api-server api-server
@@ -58,15 +49,6 @@ if [[ ! -f /etc/nethserver/registry.json ]] ; then
     echo '{"auths":{}}' > /etc/nethserver/registry.json
 fi
 
-if ! grep -q ' cluster-leader$' /etc/hosts; then
-    echo "Add /etc/hosts entries:"
-    echo "127.0.0.1 cluster-leader" >> /etc/hosts
-    echo "127.0.0.1 cluster-localnode" >> /etc/hosts
-fi
-
-echo "Generate WireGuard VPN key pair:"
-(umask 0077; wg genkey | tee /etc/nethserver/wg0.key | wg pubkey) | tee /etc/nethserver/wg0.pub
-
 echo "Add firewalld core rules:"
 (
     exec >/dev/null
@@ -74,141 +56,13 @@ echo "Add firewalld core rules:"
     firewall-cmd --reload
 )
 
-echo "Start Redis DB:"
-systemctl enable --now redis
-
-echo "Generating cluster password:"
-cluster_password=$(podman exec redis redis-cli ACL GENPASS)
-cluster_pwhash=$(echo -n "${cluster_password}" | sha256sum | awk '{print $1}')
-(umask 0077; exec >/var/lib/nethserver/cluster/state/agent.env
-    printf "AGENT_BASEACTIONS_DIR=\n"
-    printf "AGENT_ID=cluster\n" # Override value from agent@.service
-    printf "REDIS_USER=cluster\n"
-    printf "REDIS_PASSWORD=%s\n" "${cluster_password}"
-    printf "REDIS_ADDRESS=127.0.0.1:6379\n" # Override the cluster-leader /etc/hosts record
-)
 echo "Write initial cluster environment state"
 (exec > /var/lib/nethserver/cluster/state/environment
     printf "NODE_ID=1\n"
     printf "IMAGE_ID=%s\n" $(podman image inspect -f '{{.Id}}' "${CORE_IMAGE}")
 )
-
-echo "Generating api-server password:"
-apiserver_password=$(podman exec redis redis-cli ACL GENPASS)
-apiserver_pwhash=$(echo -n "${apiserver_password}" | sha256sum | awk '{print $1}')
-apiserver_jwt_secret=$(uuidgen)
-(umask 0077; exec >/etc/nethserver/api-server.env
-    printf "REDIS_PASSWORD=%s\n" "${apiserver_password}"
-    printf "REDIS_USER=api-server\n"
-    printf "REDIS_ADDRESS=127.0.0.1:6379\n" # Override the cluster-leader /etc/hosts record
-    printf "SECRET=%s\n" "${apiserver_jwt_secret}"
-)
-
-echo "Generating node password:"
-node_password=$(podman exec redis redis-cli ACL GENPASS)
-node_pwhash=$(echo -n "${node_password}" | sha256sum | awk '{print $1}')
-(umask 0077; exec >/var/lib/nethserver/node/state/agent.env
-    printf "AGENT_BASEACTIONS_DIR=\n"
-    printf "AGENT_ID=node/1\n" # Override value from agent@.service
-    printf "REDIS_USER=node/1\n"
-    printf "REDIS_PASSWORD=%s\n" "${node_password}"
-)
 printf "NODE_ID=1\n" > /var/lib/nethserver/node/state/environment
 
-(
-    # Add the keys for the cluster bootstrap
-    cat <<EOF
-SET cluster/node_sequence 1
-SET node/1/tcp_ports_sequence 20000
-EOF
-
-    # Configure default module repository
-    cat <<EOF
-HSET cluster/repository/default url ${REPOMD:-https://distfeed.nethserver.org/ns8/updates/}/ status 1 testing ${TESTING:-0}
-EOF
-
-    # Setup initial ACLs
-    cat <<EOF
-ACL SETUSER cluster ON #${cluster_pwhash} ~* &* +@all
-AUTH cluster "${cluster_password}"
-ACL SETUSER default ON nopass ~* &* nocommands +@read +@connection +subscribe +psubscribe +psync +replconf +ping
-ACL SETUSER api-server ON #${apiserver_pwhash} ~* &* nocommands +@read +@pubsub +lpush +@transaction +@connection +role +hset
-ACL SETUSER node/1 ON #${node_pwhash} resetkeys ~node/1/* resetchannels &progress/node/1/* &node/1/event/* nocommands +@read +@write +@transaction +@connection +publish
-ACL SAVE
-SAVE
-EOF
-
-    # Set default UI name
-    cat <<EOF
-SET cluster/ui_name ${CLUSTER_NAME:-''}
-EOF
-
-) | redis-cli
-
-echo "Start API server and core agents:"
-systemctl enable --now api-server.service agent@cluster.service agent@node.service
-
-echo "Grant initial permissions:"
-runagent python3 <<'EOF'
-import agent
-import cluster.grants
-rdb = agent.redis_connect(privileged=True)
-cluster.grants.grant(rdb, action_clause="*",      to_clause="owner",  on_clause='cluster')
-cluster.grants.grant(rdb, action_clause="list-*", to_clause="reader", on_clause='cluster')
-cluster.grants.grant(rdb, action_clause="get-*",  to_clause="reader", on_clause='cluster')
-cluster.grants.grant(rdb, action_clause="show-*", to_clause="reader", on_clause='cluster')
-cluster.grants.grant(rdb, action_clause="read-*", to_clause="reader", on_clause='cluster')
-cluster.grants.grant(rdb, action_clause="*",      to_clause="owner",  on_clause='node/1')
-cluster.grants.grant(rdb, action_clause="list-*", to_clause="reader", on_clause='node/1')
-cluster.grants.grant(rdb, action_clause="get-*",  to_clause="reader", on_clause='node/1')
-cluster.grants.grant(rdb, action_clause="show-*", to_clause="reader", on_clause='node/1')
-cluster.grants.grant(rdb, action_clause="read-*", to_clause="reader", on_clause='node/1')
-cluster.grants.grant(rdb, action_clause="add-public-service",  to_clause="fwadm", on_clause='node/1')
-cluster.grants.grant(rdb, action_clause="remove-public-service",  to_clause="fwadm", on_clause='node/1')
-cluster.grants.grant(rdb, action_clause="add-custom-zone",  to_clause="fwadm", on_clause='node/1')
-cluster.grants.grant(rdb, action_clause="remove-custom-zone",  to_clause="fwadm", on_clause='node/1')
-EOF
-
-for arg in "${@}"; do
-    module=$(basename ${arg} | sed s/:.*//)
-    echo "Adding module override:  ${module} -> ${arg}"
-    redis-cli hset cluster/override/modules ${module} ${arg}
-done
-
-echo "Install Traefik:"
-add-module traefik 1
-
-echo "Setting default admin password:"
-ADMIN_PASSWORD="${ADMIN_PASSWORD:-Nethesis,1234}"
-add-user --role owner --password "${ADMIN_PASSWORD}" admin
-
-cat - <<EOF
-
-NethServer 8 Scratchpad
-----------------------------------------------------------------------------
-
-Finish the cluster configuration by running one of the following procedures.
-
-A. To join this node to an already existing cluster run:
-
-      join-cluster <cluster_url> <jwt_auth>
-
-   For instance:
-
-      join-cluster https://cluster.example.com eyJhbGc...NiIsInR5c
-
-B. To initialize this node as a cluster leader run:
-
-      create-cluster <vpn_endpoint_address>:<vpn_endpoint_port> [vpn_cidr] [admin_password]
-
-   For instance:
-
-      create-cluster $(hostname -f):55820 10.5.4.0/24 ${ADMIN_PASSWORD}
-
-Finally, access the administration UI at:
-
-   https://$(hostname -f)/cluster-admin/
-
-   User: admin
-   Password: ${ADMIN_PASSWORD}
-EOF
+if [[ -z "${NS8_TWO_STEPS_INSTALL}" ]]; then
+       /var/lib/nethserver/node/install-finalize.sh "$@"
+fi
