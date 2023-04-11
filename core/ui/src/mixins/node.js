4f99483ce9e8d8eb1d9fe0ddfa00bce108cc3e39
Andrea Leardini <andre8244@gmail.com>
2023-04-11 15:58:18 +0200

    ui: remove node: show apps installed on node

--- core/ui/src/mixins/node.js
+++ core/ui/src/mixins/node.js
@@ -14,5 +14,20 @@ export default {
         },
       });
     },
+    /**
+     * Given the output of list-installed-modules, returns an object where the keys are node IDs, and the values are the list of app instances installed on that node
+     */
+    getInstalledAppsByNode(listInstalledModulesOutput) {
+      const appsByNode = {};
+
+      Object.values(listInstalledModulesOutput).forEach((instanceList) => {
+        instanceList.forEach((instance) => {
+          const nodeApps = appsByNode[instance.node] || [];
+          nodeApps.push(instance);
+          appsByNode[instance.node] = nodeApps;
+        });
+      });
+      return appsByNode;
+    },
   },
 };
