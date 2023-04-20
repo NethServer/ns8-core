a5787fa143d4a1b4eaaa8bb30b82a082efb97c55
Davide Principi <davide.principi@nethesis.it>
2023-04-20 14:53:47 +0200

    Authorize Redis ~task/* key pattern
    - Deny "default" user access to ~task/ key prefix
    - Disable @admin commands

--- core/imageroot/var/lib/nethserver/node/install-finalize.sh
+++ core/imageroot/var/lib/nethserver/node/install-finalize.sh
@@ -82,9 +82,9 @@ EOF
     cat <<EOF
 ACL SETUSER cluster ON #${cluster_pwhash} ~* &* +@all
 AUTH cluster "${cluster_password}"
-ACL SETUSER default ON nopass ~* &* nocommands +@read +@connection +subscribe +psubscribe +ping
+ACL SETUSER default ON nopass resetkeys ~cluster/* ~node/* ~module/* resetchannels &* nocommands +@read +@connection +subscribe +psubscribe -@admin
 ACL SETUSER api-server ON #${apiserver_pwhash} ~* &* nocommands +@read +@pubsub +lpush +@transaction +@connection +role +hset
-ACL SETUSER node/1 ON #${node_pwhash} resetkeys ~node/1/* resetchannels &progress/node/1/* &node/1/event/* nocommands +@read +@write +@transaction +@connection +publish +psync +replconf
+ACL SETUSER node/1 ON #${node_pwhash} resetkeys ~node/1/* ~task/node/1/* resetchannels &progress/node/1/* &node/1/event/* nocommands +@read +@write +@transaction +@connection +publish -@admin +psync +replconf
 ACL SAVE
 SAVE
 CONFIG SET masteruser node/1
