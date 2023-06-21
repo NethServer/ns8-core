8d51964dea839d6e07775e0670ae56042463191a
Stephane de Labrusse <stephdl@de-labrusse.fr>
2023-06-21 16:25:08 +0200

    drop support aioredis, use aioredis from redis

--- core/imageroot/usr/local/agent/pypkg/agent/tasks/redisclient.py
+++ core/imageroot/usr/local/agent/pypkg/agent/tasks/redisclient.py
@@ -17,8 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with NethServer.  If not, see COPYING.
 #
-
-import aioredis
+from redis import asyncio as aioredis
 import asyncio
 import json
 import os
