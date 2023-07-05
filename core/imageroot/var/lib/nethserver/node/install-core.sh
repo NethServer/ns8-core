e7c54ad71e80b316a6a2672f4984a2423c92b9f6
Stephane de Labrusse <stephdl@de-labrusse.fr>
2023-07-05 16:26:16 +0200

    For first install we must use the pythonreq3_11.txt

--- core/imageroot/var/lib/nethserver/node/install-core.sh
+++ core/imageroot/var/lib/nethserver/node/install-core.sh
@@ -41,7 +41,7 @@ fi
 echo "Setup Python virtual environment for agents:"
 core_dir=/usr/local/agent/pyenv
 python3.11 -mvenv ${core_dir} --upgrade-deps --system-site-packages
-${core_dir}/bin/pip3.11 install -r /etc/nethserver/pythonreq.txt
+${core_dir}/bin/pip3.11 install -r /etc/nethserver/pythonreq3_11.txt
 echo "/usr/local/agent/pypkg" >$(${core_dir}/bin/python3.11 -c "import sys; print(sys.path[-1] + '/pypkg.pth')")
 
 # we must use python3.11 for agent
