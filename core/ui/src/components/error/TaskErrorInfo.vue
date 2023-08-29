4fb326248bb2c5f21d88435a6f9c04a69883a2bd
Andrea Leardini <andre8244@gmail.com>
2023-08-29 14:40:43 +0200

    ui: better error visualization for subtasks

---
+++ core/ui/src/components/error/TaskErrorInfo.vue
@@ -0,0 +1,95 @@
+<!--
+  Copyright (C) 2023 Nethesis S.r.l.
+  SPDX-License-Identifier: GPL-3.0-or-later
+-->
+<template>
+  <div>
+    <h6 class="mg-top-lg mg-bottom-sm">
+      {{ taskTitle }}
+    </h6>
+    <div>
+      {{ $t("task.task_id") }}:
+      <cv-tooltip
+        alignment="center"
+        direction="top"
+        :tip="
+          justCopied
+            ? $t('common.copied_to_clipboard')
+            : $t('common.copy_to_clipboard')
+        "
+      >
+        <cv-link
+          v-clipboard:copy="task.context.id"
+          v-clipboard:success="onCopy"
+          v-clipboard:error="onCopyError"
+        >
+          {{ task.context.id }}
+        </cv-link></cv-tooltip
+      >
+    </div>
+    <div v-if="task.result && task.result.error" class="mg-top-sm">
+      <NsCodeSnippet
+        :copyTooltip="$t('common.copy_to_clipboard')"
+        :copy-feedback="$t('common.copied_to_clipboard')"
+        :feedback-aria-label="$t('common.copied_to_clipboard')"
+        :wrap-text="true"
+        :moreText="$t('common.show_more')"
+        :lessText="$t('common.show_less')"
+        light
+        >{{ task.result.error }}</NsCodeSnippet
+      >
+    </div>
+    <TaskErrorInfoHierarchy
+      v-if="task.subTasks.length"
+      :subTasks="task.subTasks"
+    >
+    </TaskErrorInfoHierarchy>
+  </div>
+</template>
+
+<script>
+import { TaskService } from "@nethserver/ns8-ui-lib";
+import TaskErrorInfoHierarchy from "./TaskErrorInfoHierarchy";
+
+export default {
+  name: "TaskErrorInfo",
+  mixins: [TaskService],
+  components: { TaskErrorInfoHierarchy },
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      justCopied: false,
+    };
+  },
+  computed: {
+    taskTitle() {
+      const agent = this.task.context.queue
+        ? this.task.context.queue.split("/tasks")[0]
+        : "cluster";
+
+      return `${agent}/${this.task.context.action}`;
+    },
+  },
+  methods: {
+    onCopy() {
+      this.justCopied = true;
+
+      setTimeout(() => {
+        this.justCopied = false;
+      }, 3000);
+    },
+    onCopyError() {
+      console.error("cannot copy to clipboard");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../styles/carbon-utils";
+</style>
--- core/ui/src/components/error/TaskErrorModal.vue
+++ core/ui/src/components/error/TaskErrorModal.vue
@@ -19,41 +19,40 @@
         />
         <div>
           <span v-html="getTaskStatusDescription(taskErrorToShow, true)"></span>
-          <span v-if="isMoreInfoShown"
-            >. ID:
-            <cv-tooltip
-              alignment="center"
-              direction="bottom"
-              :tip="
-                justCopied
-                  ? $t('common.copied_to_clipboard')
-                  : $t('common.copy_to_clipboard')
-              "
-            >
-              <cv-link
-                v-clipboard:copy="taskErrorToShow.context.id"
-                v-clipboard:success="onCopy"
-                v-clipboard:error="onCopyError"
-              >
-                {{ taskErrorToShow.context.id }}
-              </cv-link></cv-tooltip
-            >
-          </span>
         </div>
       </div>
       <div v-if="subTasks.length">
-        <TaskHierarchy
+        <TaskTreeHierarchy
           :subTasks="subTasks"
           :isMoreInfoShown="isMoreInfoShown"
           class="task-hierarchy"
         />
       </div>
+      <!-- copy task json task data to clipboard -->
+      <cv-tooltip
+        alignment="center"
+        direction="right"
+        :tip="
+          justCopied
+            ? $t('common.copied_to_clipboard')
+            : $t('common.copy_to_clipboard')
+        "
+        class="mg-top-md"
+      >
+        <cv-link
+          v-clipboard:copy="JSON.stringify(taskErrorToShow)"
+          v-clipboard:success="onCopy"
+          v-clipboard:error="onCopyError"
+        >
+          {{ $t("task.copy_task_data_to_clipboard") }}
+        </cv-link></cv-tooltip
+      >
       <cv-toggle
         value="moreInfoValue"
         :form-item="true"
         v-model="isMoreInfoShown"
         hide-label
-        class="mg-bottom-md"
+        class="mg-top-sm mg-bottom-md"
       >
         <template slot="text-left">{{ $t("common.more_info") }}</template>
         <template slot="text-right">{{ $t("common.more_info") }}</template>
@@ -84,33 +83,7 @@
             </template>
           </cv-interactive-tooltip>
         </span>
-        <div>
-          <NsCodeSnippet
-            :copyTooltip="$t('common.copy_to_clipboard')"
-            :copy-feedback="$t('common.copied_to_clipboard')"
-            :feedback-aria-label="$t('common.copied_to_clipboard')"
-            :wrap-text="true"
-            :moreText="$t('common.show_more')"
-            :lessText="$t('common.show_less')"
-            light
-            >{{ getTaskError(taskErrorToShow) }}</NsCodeSnippet
-          >
-        </div>
-        <h6 class="mg-top-md mg-bottom-sm">
-          {{ $t("common.task_data") }}
-        </h6>
-        <div>
-          <NsCodeSnippet
-            :copyTooltip="$t('common.copy_to_clipboard')"
-            :copy-feedback="$t('common.copied_to_clipboard')"
-            :feedback-aria-label="$t('common.copied_to_clipboard')"
-            :wrap-text="true"
-            :moreText="$t('common.show_more')"
-            :lessText="$t('common.show_less')"
-            light
-            >{{ taskErrorToShow }}</NsCodeSnippet
-          >
-        </div>
+        <TaskErrorInfo :task="taskErrorToShow" />
       </template>
     </template>
     <template v-if="isLogAvailable">
@@ -129,7 +102,9 @@
 
 <script>
 import { mapState } from "vuex";
-import TaskHierarchy from "./TaskHierarchy";
+import TaskTreeHierarchy from "./TaskTreeHierarchy";
+import TaskErrorInfoHierarchy from "./TaskErrorInfoHierarchy";
+import TaskErrorInfo from "./TaskErrorInfo";
 import {
   TaskService,
   StorageService,
@@ -138,7 +113,7 @@ import {
 
 export default {
   name: "TaskErrorModal",
-  components: { TaskHierarchy },
+  components: { TaskTreeHierarchy, TaskErrorInfoHierarchy, TaskErrorInfo },
   mixins: [TaskService, StorageService, IconService],
   props: {},
   data() {
