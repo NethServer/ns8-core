35eeac0a1dc0a8a53202ea0a02488b60a402032b
Stefano <scarpettastefano.main@gmail.com>
2024-02-27 15:19:03 +0000

    fix: disable deletion of admins and domain admins group

--- core/ui/src/components/domains/DomainGroups.vue
+++ core/ui/src/components/domains/DomainGroups.vue
@@ -101,6 +101,7 @@
                       <NsMenuDivider />
                       <cv-overflow-menu-item
                         danger
+                        :disabled="row.group === 'domain admins'"
                         @click="showDeleteGroupModal(row)"
                         :data-test-id="row.group + '-delete'"
                       >
