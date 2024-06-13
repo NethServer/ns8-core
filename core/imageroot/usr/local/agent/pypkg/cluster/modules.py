3414c4383087cd068340ee97cf5194b207b634bb
Davide Principi <davide.principi@nethesis.it>
2024-06-13 18:41:03 +0200

    Consider installed instances as an app repository
    Even if all repositories are disabled, consider installed instances as a
    repository itself and return synthesized metadata from list_available().
    
    An installed application is always properly covered by its metadata.json and
    image labels as fallback, in case a remote repository does not list it
    any more.

--- core/imageroot/usr/local/agent/pypkg/cluster/modules.py
+++ core/imageroot/usr/local/agent/pypkg/cluster/modules.py
@@ -29,6 +29,8 @@ import requests, urllib3.util
 import hashlib
 from glob import glob
 import sys
+import subprocess
+import datetime
 
 def _urljoin(base_path, *args):
     '''replace urllib.parse.joinurl because it doesn't handle multiple parameters
@@ -148,7 +150,7 @@ def get_latest_module(module, rdb):
     for m in available:
         if m["id"] == module:
             source = m["source"]
-            repo_testing = int(rdb.hget(f'cluster/repository/{m["repository"]}', 'testing')) == 1
+            repo_testing = rdb.hget(f'cluster/repository/{m["repository"]}', 'testing') == "1"
             for v in m["versions"]:
                 if repo_testing:
                     version = v["tag"]
@@ -175,6 +177,67 @@ def get_version_tuple(v):
         vinfo = (0,)
     return vinfo
 
+def _synthesize_module_version(minstance):
+    """From an installed module instance, synthesize module version for
+    repository metadata, or fall back to a safe default."""
+    inspect_labels = {}
+    image_url = minstance["source"] + ':' + minstance["version"]
+    # Extract image labels
+    try:
+        with subprocess.Popen(['podman', 'image', 'inspect', image_url], stdout=subprocess.PIPE, stderr=sys.stderr) as proc:
+            inspect = json.load(proc.stdout)
+            inspect_labels = inspect[0]['Labels']
+    except Exception as ex:
+        print(agent.SD_WARNING + "_synthesize_module_version:", ex, file=sys.stderr)
+    # Extract the prerelease flag:
+    try:
+        vinfo = semver.VersionInfo(minstance["version"])
+        testing = vinfo.prerelease
+    except:
+        testing = False
+    inspect_labels.setdefault("org.nethserver.images", image_url)
+    return {
+        "tag": minstance["version"],
+        "testing": testing,
+        "labels": inspect_labels,
+    }
+
+def _fetch_metadata_json(module_id, image_name):
+    """Synthesize repository metadata from the local metadata.json
+    file, or fall back to safe defaults."""
+    path_prefix = '/var/lib/nethserver/cluster/ui/'
+    repository_updated_timestamp = 'Thu Jun 13 01:00:00 AM UTC 2024'
+    try:
+        with open(f"{path_prefix}apps/{module_id}/metadata.json") as mfd:
+            # Read modification timestamp of the .json file:
+            repository_updated_timestamp = datetime.datetime.utcfromtimestamp(os.fstat(mfd.fileno()).st_mtime).ctime()
+            ometadata = json.load(mfd)
+    except Exception as ex:
+        print(agent.SD_INFO + "_fetch_metadata_json/open:", ex, file=sys.stderr)
+        ometadata = {
+            "id": image_name,
+            "name": image_name.capitalize(),
+            "description": {"en": ""},
+            "categories": [],
+            "logo": "",
+            "authors": [],
+            "docs": {
+                "documentation_url": "",
+                "code_url": "",
+                "bug_url": "",
+            },
+        }
+    ometadata.setdefault("screenshots", [])
+    ometadata.setdefault("repository", "__local__")
+    ometadata.setdefault("testing", "0")
+    ometadata.setdefault("repository_updated", repository_updated_timestamp)
+    try:
+        ometadata['logo'] = glob(f'{path_prefix}apps/{module_id}/img/*logo*png')[0].removeprefix(path_prefix)
+    except Exception as ex:
+        ometadata['logo'] = ""
+        print(agent.SD_INFO + "_fetch_metadata_json/glob:", ex, file=sys.stderr)
+    return ometadata
+
 def list_available(rdb, skip_core_modules = False):
     """Iterate over enabled repositories and return available modules respecting the repository priority."""
     modules = {}
@@ -195,6 +258,19 @@ def list_available(rdb, skip_core_modules = False):
                 continue # skip duplicated images from lower priority modules
             modules[rmod["source"]] = rmod
             rmod['versions'].sort(key=lambda v: get_version_tuple(v["tag"]), reverse=True)
+    # Integrate the available set with instances that do not belong to any
+    # repository. They can be found in the "installed" dict:
+    for module_source, module_instances in list_installed(rdb, skip_core_modules).items():
+        if module_source in modules:
+            continue
+        _, image_name = module_source.rsplit("/", 1)
+        vmetadata = _fetch_metadata_json(module_instances[0]['id'], image_name)
+        vmetadata["versions"] = list(_synthesize_module_version(oinst) for oinst in module_instances)
+        vmetadata["installed"] = module_instances
+        vmetadata["source"] = module_source
+        vmetadata["updates"] = []
+        vmetadata["id"] = image_name
+        modules[module_source] = vmetadata
     return list(modules.values())
 
 def list_installed(rdb, skip_core_modules = False):
@@ -249,15 +325,17 @@ def list_updates(rdb, skip_core_modules = False):
     for module in available:
         if module["source"] not in installed.keys():
             continue
-
         newest_version = None
         for version in module["versions"]:
-            v = semver.VersionInfo.parse(version["tag"])
+            try:
+                # skip bogus version tag
+                v = semver.VersionInfo.parse(version["tag"])
+            except:
+                continue
             # Skip testing versions if testing is disabled
             testing = rdb.hget(f'cluster/repository/{module["repository"]}', 'testing')
-            if int(testing) == 0 and not v.prerelease is None:
+            if testing != "1" and not v.prerelease is None:
                 continue
-
             newest_version = version["tag"]
             break
 
