73e03177f539998e7a8da336a6dfcaf5fde410a9
Andrea Leardini <andre8244@gmail.com>
2024-07-03 16:22:15 +0200

    fix: hide app description in sw center

--- core/ui/src/components/software-center/AppList.vue
+++ core/ui/src/components/software-center/AppList.vue
@@ -54,14 +54,19 @@
                 }}</a>
                 <span v-else>{{ app.name }}</span>
               </div>
-              <div class="app-description app-row">
-                {{ getApplicationDescription(app) }}
-              </div>
               <div
-                v-if="app.categories && getApplicationCategories(app)"
+                v-if="app.categories && getApplicationCategories(app).length"
                 class="app-categories app-row"
               >
-                {{ getApplicationCategories(app) }}
+                <span
+                  v-for="(category, index) in getApplicationCategories(app)"
+                  :key="category"
+                >
+                  {{ category }}
+                  <span v-if="index < getApplicationCategories(app).length - 1"
+                    >&#8226;</span
+                  >
+                </span>
               </div>
               <div v-if="app.id == 'core'" class="app-row">
                 <NsButton
@@ -217,11 +222,16 @@ export default {
         }
       }
     },
-    getApplicationDescription(app) {
-      return this.getAppDescription(app, this);
-    },
     getApplicationCategories(app) {
-      return this.getAppCategories(app, this);
+      let categories = [];
+
+      for (const category of app.categories) {
+        if (category === "unknown") {
+          continue;
+        }
+        categories.push(this.$t("software_center.app_categories." + category));
+      }
+      return categories;
     },
     goToSoftwareCenterAppInstances(app) {
       this.$router.push({
@@ -277,12 +287,6 @@ export default {
 
 .app-name {
   font-weight: bold;
-}
-
-.app-description {
-  color: $text-02;
-  text-align: center;
-  line-height: 1.5;
 }
 
 .app-categories {
