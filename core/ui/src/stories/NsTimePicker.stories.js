b982f8a2d3ec7a39eebe1f4395b4f8f297d94ed2
Tommaso Ascani <tommaso.ascani@gmail.com>
2024-10-18 17:32:51 +0200

    Storybook update

--- core/ui/src/stories/NsTimePicker.stories.js
+++ core/ui/src/stories/NsTimePicker.stories.js
@@ -5,12 +5,19 @@ export default {
   component: NsTimePicker,
 };
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
+const Template = (args) => ({
+  props: ['value', 'label', 'hideClearButton', 'invalidMessage', 'light'], // Props specifiche per Template
   components: { NsTimePicker },
   template: '<NsTimePicker v-bind="$props" />',
 });
 
+const Template2 = (args) => ({
+  props: ['value', 'label', 'hideClearButton', 'invalidMessage', 'light', 'dropDirection'], // Props specifiche per Template2
+  components: { NsTimePicker },
+  template: '<NsTimePicker v-bind="$props" style="margin-top: 400px"/>',
+});
+
+// Esportazione per il componente Default senza dropDirection
 export const Default = Template.bind({});
 Default.args = {
   value: "23:45",
@@ -20,11 +27,21 @@ Default.args = {
   light: true,
 };
 
-export const Drop_Up = Template.bind({});
+// Esportazione per Drop_Up con controllo radio per dropDirection
+export const Drop_Up = Template2.bind({});
 Drop_Up.args = {
   value: "23:45",
   label: "Label",
   hideClearButton: false,
   invalidMessage: "",
   light: true,
+  dropDirection: 'up',
+};
+Drop_Up.argTypes = {
+  dropDirection: {
+    control: {
+      type: 'radio',
+      options: ['up', 'down', 'auto'],
+    },
+  },
 };
