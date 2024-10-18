f3bb76004d5e1ca64ff1273fd0d34155660b6a70
Tommaso Ascani <tommaso.ascani@gmail.com>
2024-10-18 14:41:10 +0200

    New clm/syslog log filters

--- core/ui/src/components/settings/CloudLogManagerConfigureModal.vue
+++ core/ui/src/components/settings/CloudLogManagerConfigureModal.vue
@@ -54,6 +54,25 @@
             ></span>
           </template>
         </NsTextInput>
+        <div v-if="toggleEnabled">
+          <label for="filter" :class="`${carbonPrefix}--label`">{{
+            $t("cloud_log_manager_forwarder.filter")
+          }}</label>
+          <cv-radio-group :vertical="true">
+            <cv-radio-button
+              ref="filterVal"
+              :label="$t('cloud_log_manager_forwarder.full')"
+              value="full"
+              v-model="filterVal"
+            />
+            <cv-radio-button
+              ref="filterVal"
+              :label="$t('cloud_log_manager_forwarder.root')"
+              value="root"
+              v-model="filterVal"
+            />
+          </cv-radio-group>
+        </div>
         <div v-if="toggleEnabled && this.configuration.last_timestamp != ''">
           <label for="startTime" :class="`${carbonPrefix}--label`">{{
             $t("cloud_log_manager_forwarder.export_starting_date")
@@ -156,6 +175,7 @@ export default {
     return {
       radioVal: "last_timestamp",
       toggleEnabled: false,
+      filterVal: "full",
       address: "",
       tenant: "",
       clusterId: "",
@@ -183,6 +203,7 @@ export default {
     resetModal() {
       this.radioVal = "last_timestamp";
       this.toggleEnabled = false;
+      this.filterVal = "full";
       this.address = "";
       this.tenant = "";
       this.date = "";
@@ -211,6 +232,10 @@ export default {
 
       if (!this.configuration.tenant == "") {
         this.tenant = this.configuration.tenant;
+      }
+
+      if (this.configuration.filter != "") {
+        this.filterVal = this.configuration.filter;
       }
 
       if (this.configuration.cluster_id) {
@@ -282,6 +307,7 @@ export default {
             active: this.active,
             address: this.address,
             tenant: this.tenant,
+            filter: this.filterVal,
             start_time: this.start_time,
           },
           extra: {
