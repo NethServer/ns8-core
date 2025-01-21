c244416929e8b13f6e97cf9521a5b7a7e3f2ff7f
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-01-21 13:04:02 +0100

    fix(phonehome): change script names
    Add a new command to print the phonehome output:
    
      runagent print-phonehome

--- core/imageroot/usr/local/agent/pypkg/cluster/inventory.py
+++ core/imageroot/usr/local/agent/pypkg/cluster/inventory.py
@@ -7,18 +7,29 @@ import sys
 import cluster.modules
 import agent
 
-def subscription_fact():
+def fact_subscription():
     ret = {"subscription": ""}
     rdb = agent.redis_connect(use_replica=True)
     provider = rdb.hget('cluster/subscription', 'provider')
     if provider == "nsent":
-        ret["subscription"] = "enterprise"
+        return "enterprise"
     elif provider == "nscom":
-        ret["subscription"] = "community"
+        return "community"
     else:
-        ret["subscription"] = "disabled"
+        return "disabled"
 
-    return ret
+
+def fact_admins():
+    rdb = agent.redis_connect(privileged=True)
+    users = {"count": 0, "2fa": 0}
+    # get all user keys
+    for ukey in rdb.scan_iter('user/*'):
+        users["count"] += 1
+        two_fa = rdb.hget(ukey, '2fa')
+        if two_fa:
+            users["2fa"] += 1
+    return users
+
 
 def cluster_facts():
     result = agent.tasks.run(
@@ -46,36 +57,39 @@ def modules_facts():
     for module in instance_list:
         minfo = {"id": module.get('id'), "version": module.get("version"), "name": module.get("module"), "node": module.get("node") } 
 
-        list_actions_result = agent.tasks.run(
-            agent_id='module/' + module.get('id'),
-            action='list-actions',
-            extra={
-                'isNotificationHidden': True,
-            },
-            endpoint="redis://cluster-leader",
-        )
-
-        if not list_actions_result or list_actions_result['exit_code'] != 0:
-            print(agent.SD_WARNING + f"module/{module.get('id')}/list-actions failed", file=sys.stderr)
-            return None
-        
-        if 'get-facts' in list_actions_result.get('output', []):
-
-            get_facts_result = agent.tasks.run(
+        try:
+            list_actions_result = agent.tasks.run(
                 agent_id='module/' + module.get('id'),
-                action='get-facts',
-                extra={ 'isNotificationHidden': True },
+                action='list-actions',
+                extra={
+                    'isNotificationHidden': True,
+                },
                 endpoint="redis://cluster-leader",
             )
 
-            if get_facts_result['exit_code'] != 0:
-                print(agent.SD_WARNING, f"get-facts failed for {module.get('id')}", file=sys.stderr)
-                continue
+            if not list_actions_result or list_actions_result['exit_code'] != 0:
+                print(agent.SD_WARNING + f"module/{module.get('id')}/list-actions failed", file=sys.stderr)
+                raise Exception("list-actions failed")
+            
+            if 'get-facts' in list_actions_result.get('output', []):
 
-            ofacts = get_facts_result['output']
-            # merge info from get-facts
-            minfo.update(ofacts)
+                get_facts_result = agent.tasks.run(
+                    agent_id='module/' + module.get('id'),
+                    action='get-facts',
+                    extra={ 'isNotificationHidden': True },
+                    endpoint="redis://cluster-leader",
+                )
 
-        ret.append(minfo)
+                if get_facts_result['exit_code'] != 0:
+                    print(agent.SD_WARNING, f"get-facts failed for {module.get('id')}", file=sys.stderr)
+                    raise Exception("get-facts failed")
+
+                ofacts = get_facts_result['output']
+                # merge info from get-facts
+                minfo.update(ofacts)
+        except:
+            pass
+        finally:
+            ret.append(minfo)
 
     return ret
