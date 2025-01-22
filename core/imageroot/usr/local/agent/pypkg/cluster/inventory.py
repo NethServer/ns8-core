57bee95e82e030682700538bc32c887242d719c4
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-01-22 11:18:44 +0100

    fix(phonehome): refactor task calls
    All task calls are now handled directly from print-phonehome script

--- core/imageroot/usr/local/agent/pypkg/cluster/inventory.py
+++ core/imageroot/usr/local/agent/pypkg/cluster/inventory.py
@@ -3,13 +3,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-import sys
-import cluster.modules
+import os
 import agent
 
-def fact_subscription():
-    ret = {"subscription": ""}
-    rdb = agent.redis_connect(use_replica=True)
+def fact_subscription(rdb):
+    # Get subscription status
     provider = rdb.hget('cluster/subscription', 'provider')
     if provider == "nsent":
         return "enterprise"
@@ -18,11 +16,9 @@ def fact_subscription():
     else:
         return "disabled"
 
-
-def fact_admins():
-    rdb = agent.redis_connect(privileged=True)
+def fact_admins(rdb):
+    # Count admin users and users with 2FA enabled
     users = {"count": 0, "2fa": 0}
-    # get all user keys
     for ukey in rdb.scan_iter('user/*'):
         users["count"] += 1
         two_fa = rdb.hget(ukey, '2fa')
@@ -30,66 +26,22 @@ def fact_admins():
             users["2fa"] += 1
     return users
 
-
-def cluster_facts():
-    result = agent.tasks.run(
-        agent_id='cluster',
-        action='get-facts',
-        extra={
-            'isNotificationHidden': True,
-        },
-        endpoint="redis://cluster-leader"
-    )
-
-    if result['exit_code'] == 0:
-        return result['output']
-    else:
-        print(agent.SD_ERR + f"cluster/get-facts failed", file=sys.stderr)
-        return None
- 
-def modules_facts():
+def fact_repositories(rdb):
+    # List enabled repositories
     ret = []
-    rdb = agent.redis_connect(use_replica=True)
-    installed_modules = cluster.modules.list_installed(rdb, skip_core_modules = False)
-    instance_list = list(mi for module_instances in installed_modules.values() for mi in module_instances)
-
-    # Collect facts from modules:
-    for module in instance_list:
-        minfo = {"id": module.get('id'), "version": module.get("version"), "name": module.get("module"), "node": module.get("node") } 
-
-        try:
-            list_actions_result = agent.tasks.run(
-                agent_id='module/' + module.get('id'),
-                action='list-actions',
-                extra={
-                    'isNotificationHidden': True,
-                },
-                endpoint="redis://cluster-leader",
-            )
-
-            if not list_actions_result or list_actions_result['exit_code'] != 0:
-                print(agent.SD_WARNING + f"module/{module.get('id')}/list-actions failed", file=sys.stderr)
-                raise Exception("list-actions failed")
-            
-            if 'get-facts' in list_actions_result.get('output', []):
-
-                get_facts_result = agent.tasks.run(
-                    agent_id='module/' + module.get('id'),
-                    action='get-facts',
-                    extra={ 'isNotificationHidden': True },
-                    endpoint="redis://cluster-leader",
-                )
-
-                if get_facts_result['exit_code'] != 0:
-                    print(agent.SD_WARNING, f"get-facts failed for {module.get('id')}", file=sys.stderr)
-                    raise Exception("get-facts failed")
-
-                ofacts = get_facts_result['output']
-                # merge info from get-facts
-                minfo.update(ofacts)
-        except:
-            pass
-        finally:
-            ret.append(minfo)
-
+    for m in rdb.scan_iter('cluster/repository/*'):
+        repo = rdb.hgetall(m)
+        if repo.get("status") == "1":
+            ret.append(os.path.basename(m))
     return ret
+
+def fact_smarthost(rdb):
+    # Get smarthost configuration
+    manual_configuration = True
+    smtp = agent.get_smarthost_settings(rdb)
+    # we query all the list of service provider
+    for key in agent.list_service_providers(rdb,'submission','tcp'):
+        if manual_configuration == True:
+            # no need to check the rest of the list if manual_configuration == False
+            manual_configuration = False if key['host'] == smtp['host'] and str(smtp['port']) == "25" else True
+    return{"enabled": smtp["enabled"], "manual_configuration": manual_configuration}
