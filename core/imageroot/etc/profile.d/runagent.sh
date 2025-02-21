06c1fe7a6f36183dc57055b4292a8a908b3c2445
Davide Principi <davide.principi@nethesis.it>
2025-02-21 12:13:04 +0100

    feat(runagent): list running agent IDs (#789)
    Press TAB to complete -m/--module-id option with agent IDs.

--- core/imageroot/etc/profile.d/runagent.sh
+++ core/imageroot/etc/profile.d/runagent.sh
@@ -10,3 +10,35 @@ if [ -n "${AGENT_STATE_DIR}" ] && [ -f "${AGENT_STATE_DIR}/agent.env" ]; then
    . ${AGENT_STATE_DIR}/agent.env
    export PATH
 fi
+
+if [ -z "$BASH" ]; then
+    return
+fi
+
+_runagent_wastyped()
+{
+    local xword checkword="$1"
+    for xword in "${COMP_WORDS[@]}"; do
+        if [[ "${xword}" == "${checkword}" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+_runagent_completions()
+{
+    local cword="$2"
+    local agents=()
+
+    if _runagent_wastyped "-l" || _runagent_wastyped "--list-modules"; then
+        :
+    elif _runagent_wastyped "-m" || _runagent_wastyped "--module-id" ; then
+        agents+=($(runagent --list-modules | sort | grep ^"${cword}"))
+        COMPREPLY+=($(compgen -W "${agents[*]}" -- "${cword}"))
+    else
+        COMPREPLY+=($(compgen -W "--list-modules --module-id --current-dir --help" -- "${cword}"))
+    fi
+}
+
+complete -F _runagent_completions runagent
