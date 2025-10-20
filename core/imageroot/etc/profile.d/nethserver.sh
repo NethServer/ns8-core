d5ad2eceb32cfe1e5f8d96944fac509c296df9e9
Davide Principi <davide.principi@nethesis.it>
2025-10-20 14:31:02 +0200

    feat: enable Bash history timestamps (#967)
    - Add timestamps to Bash history
    - Limit history to 2000 entries

---
+++ core/imageroot/etc/profile.d/nethserver.sh
@@ -0,0 +1,13 @@
+
+#
+# Copyright (C) 2025 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+if [ -z "$BASH" ]; then
+    return
+fi
+
+export HISTSIZE=2000
+export HISTFILESIZE=2000
+export HISTTIMEFORMAT="%F %T "
