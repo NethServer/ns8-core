3bb7d9570e6b6a6e59dbd94abe8dd4605efca3e2
Davide Principi <davide.principi@nethesis.it>
2025-11-07 18:04:43 +0100

    feat: configurable volume mountpoints
    - Read default additional directories from optional config file
      /etc/nethserver/volumes.conf
    
    - Pass volumes configuration to cluster/add-module action and validate
      it.
    
    - Pass volumes configuration to cluster/clone-module and
      cluster/restore-module actions.
    
    - Pre-create additional directories for application volumes in node's
      add-module.
    
    - Pre-create application volumes during create-module, with volume bind
      mount to additional directories.
    
    - Implement node's list-mountpoints action.

---
+++ core/imageroot/usr/local/agent/pypkg/agent/volumes.py
@@ -0,0 +1,69 @@
+#
+# Copyright (C) 2025 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+import sys
+import subprocess
+import json
+from configparser import ConfigParser
+
+def _parse_config() -> ConfigParser:
+    config = ConfigParser(
+        delimiters=("="),
+        comment_prefixes=("#"),
+        inline_comment_prefixes=("#"),
+        empty_lines_in_values=False,
+    )
+    try:
+        config.read("/etc/nethserver/volumes.conf")
+    except FileNotFoundError:
+        pass
+    return config
+
+def get_configuration(module_id:str) -> dict:
+    """Return the volume configuration for the given module_id by reading
+    the config file /etc/nethserver/volumes.conf."""
+    app_type = module_id.rstrip('0123456789')
+    oconfig = _parse_config()
+    if module_id in oconfig:
+        section_key = module_id
+    elif app_type in oconfig:
+        section_key = app_type
+    else:
+        return {} # config not found
+    # Check if the paths are valid mountpoints
+    valid_paths = [bp["path"] for bp in get_base_paths()]
+    volmap = {}
+    for volname, volpath in oconfig[section_key].items():
+        volpath = volpath.rstrip("/")
+        if volpath in valid_paths:
+            volmap[volname] = volpath
+        else:
+            print(f"volumes.py: invalid base path {volpath} (volume '{volname}', app '{section_key}')", file=sys.stderr)
+    return volmap
+
+def get_base_paths() -> list:
+    with subprocess.Popen(["findmnt", "--real", "--json", "--bytes", "--output", "SOURCE,TARGET,SIZE,USED,LABEL", "--nofsroot", "--type", "nofat,novfat"], stdout=subprocess.PIPE) as hproc:
+        ofindmnt = json.load(hproc.stdout)
+    orootfs = ofindmnt["filesystems"][0]
+    paths = []
+    for ofs in orootfs.get("children", []):
+        if ofs["source"] == orootfs["source"]:
+            continue # ignore bind mounts
+        elif ofs["target"].startswith("/boot"):
+            continue # ignore /boot* mountpoints
+        opath = {
+            "path": ofs["target"],
+            "label": ofs["label"] or "",
+        }
+        if "size" in ofs:
+            opath["size"] = ofs["size"]
+            opath["used"] = ofs["used"]
+            opath["available"] = ofs["size"] - ofs["used"]
+        paths.append(opath)
+    return paths
+
+if __name__ == "__main__":
+    json.dump(get_base_paths(), fp=sys.stdout)
+
