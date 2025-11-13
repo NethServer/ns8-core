1981b22cb61ea8d306ab0af9a98f194d6d1dc4d7
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-11-13 14:14:35 +0100

    feat(ui): add RestartModuleModal component for application restart confirmation

---
+++ core/ui/src/components/software-center/RestartModuleModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <NsModal
+    size="default"
+    :visible="visible"
+    @modal-hidden="$emit('hide')"
+    @primary-click="$emit('primary-click')"
+  >
+    <template slot="title">
+      {{
+        $t("applications.app_restart", {
+          app: instanceToRestart ? instanceToRestart.id : "",
+        })
+      }}
+    </template>
+    <template slot="content">
+      <NsInlineNotification
+        kind="warning"
+        :title="$t('common.please_read_carefully')"
+        :description="$t('applications.restart_module_warning')"
+        :showCloseButton="false"
+      />
+      <div>
+        {{
+          $t("applications.restart_app", {
+            name: instanceToRestart ? instanceToRestart.id : "",
+          })
+        }}
+      </div>
+    </template>
+    <template slot="secondary-button">{{ $t("common.cancel") }}</template>
+    <template slot="primary-button">{{ $t("applications.restart_instance") }}</template>
+  </NsModal>
+</template>
+
+<script>
+export default {
+  name: "RestartModuleModal",
+  props: {
+    visible: {
+      type: Boolean,
+      required: true
+    },
+    instanceToRestart: {
+      type: Object,
+      default: null
+    }
+  }
+};
+</script>
