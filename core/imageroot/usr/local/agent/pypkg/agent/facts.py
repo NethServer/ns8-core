7c2fce35f9729d340c51c8646cb80d256c0c65d2
Davide Principi <davide.principi@nethesis.it>
2026-01-22 18:23:33 +0100

    feat(print-phonehome): string pseudonymization
    - Initialize a random seed for string pseudonymization. Save it into
      Redis for stable results.
    
    - Enforce pseudonymization if a subscription is not active.

---
+++ core/imageroot/usr/local/agent/pypkg/agent/facts.py
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2026 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+# Fact collection helpers
+
+import hashlib
+
+PSEUDO_ENFORCING = True
+PSEUDO_SEED = '0000'
+
+def init_pseudonymization(enforce, rdb):
+    global PSEUDO_ENFORCING, PSEUDO_SEED
+    seed = rdb.get('cluster/anon_seed')
+    if not seed:
+        raise Exception("The cluster/anon_seed value is not set")
+    PSEUDO_ENFORCING = enforce
+    PSEUDO_SEED = seed
+
+def pseudo_string(val, maxlen=12):
+    """Calculate a stable pseudonym of the given string"""
+    if val and PSEUDO_ENFORCING:
+        hashed_val = hashlib.md5((PSEUDO_SEED + val).encode('utf-8')).hexdigest()
+        return hashed_val[0:maxlen]
+    else:
+        return val
