6960b8dd24181a9acc8ee935f2774da9bbf96b17
Stephane de Labrusse <stephdl@de-labrusse.fr>
2026-01-29 08:24:30 +0000

    feat(AddInternalProviderModal): remove skeleton loader for module list during loading

--- core/ui/src/components/domains/AddInternalProviderModal.vue
+++ core/ui/src/components/domains/AddInternalProviderModal.vue
@@ -52,13 +52,6 @@
           :description="error.listModules"
           :showCloseButton="false"
         />
-        <cv-skeleton-text
-          v-else-if="loading.listModules"
-          :paragraph="true"
-          :line-count="5"
-          heading
-          class="mg-top-xlg"
-        ></cv-skeleton-text>
         <NodeSelector
           v-else
           :disabledNodes="disabledNodes"
