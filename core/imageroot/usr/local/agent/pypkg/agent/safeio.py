6bee7a99bc4ef09b4f7f18afce18712ffa8d2f17
Davide Principi <davide.principi@nethesis.it>
2026-03-10 09:05:59 +0000

    fix: preserve file permissions in safe_open()
    - If the file is new, generate a unique temporary file name and write to
      it. Use os.open() to honor the process umask.
    
    - If the file already exists, copy its permissions to a temporary file
      created with tempfile.mkstemp().

--- core/imageroot/usr/local/agent/pypkg/agent/safeio.py
+++ core/imageroot/usr/local/agent/pypkg/agent/safeio.py
@@ -4,6 +4,7 @@
 #
 
 import os
+import stat
 import fcntl
 import tempfile
 from contextlib import contextmanager
@@ -14,16 +15,36 @@ def safe_open(path, mode="w", encoding=None):
         raise ValueError("Only write modes supported")
 
     directory = os.path.dirname(path) or "."
+    filename = os.path.basename(path)
 
     fd = None
     f = None
     tmp_path = None
     try:
-        # Unique temp file in same directory
-        fd, tmp_path = tempfile.mkstemp(
-            prefix="." + os.path.basename(path) + ".tmp-",
-            dir=directory
-        )
+        try:
+            # Read original file permissions
+            ref_mode = stat.S_IMODE(os.stat(path).st_mode)
+            # Unique temp file in same directory
+            fd, tmp_path = tempfile.mkstemp(
+                prefix="." + filename + ".tmp-",
+                dir=directory
+            )
+            # Copy original file permissions to temp file
+            os.fchmod(fd, ref_mode)
+        except FileNotFoundError:
+            attempt = 0
+            base_flags = os.O_RDWR if '+' in mode else os.O_WRONLY
+            while True:
+                # Generate a unique temporary file name
+                tmp_path = os.path.join(directory, ".{0}.tmp-{1}-{2}".format(filename, os.getpid(), attempt))
+                try:
+                    # Try to create a new file on tmp_path. Kernel
+                    # applies current process umask on file creation
+                    fd = os.open(tmp_path, base_flags | os.O_CREAT | os.O_EXCL, 0o666)
+                    break # exit retry-loop
+                except FileExistsError:
+                    # tmp_path is not unique, try a different path
+                    attempt += 1
 
         if "b" in mode:
             f = os.fdopen(fd, mode)
